818ba52d41c046532e16aaddbdb17123598ad2ef
joy <joy.jishnu.10@gmail.com>
2018-04-06 07:08:15 +0530

    Created train, predict, simulate operators

---
+++ framework-service/src/main/java/com/inferyx/framework/operator/PredictMLOperator.java
@@ -0,0 +1,151 @@
+/**
+ * 
+ */
+package com.inferyx.framework.operator;
+
+import java.util.List;
+
+import org.apache.spark.SparkContext;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.storage.StorageLevel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.inferyx.framework.common.HDFSInfo;
+import com.inferyx.framework.common.MetadataUtil;
+import com.inferyx.framework.connector.ConnectionHolder;
+import com.inferyx.framework.connector.IConnector;
+import com.inferyx.framework.domain.Algorithm;
+import com.inferyx.framework.domain.Attribute;
+import com.inferyx.framework.domain.DataStore;
+import com.inferyx.framework.domain.Datapod;
+import com.inferyx.framework.domain.Datasource;
+import com.inferyx.framework.domain.MetaIdentifierHolder;
+import com.inferyx.framework.domain.MetaType;
+import com.inferyx.framework.domain.Model;
+import com.inferyx.framework.domain.Predict;
+import com.inferyx.framework.domain.ResultSetHolder;
+import com.inferyx.framework.domain.TrainExec;
+import com.inferyx.framework.executor.IExecutor;
+import com.inferyx.framework.factory.ConnectionFactory;
+import com.inferyx.framework.factory.DataSourceFactory;
+import com.inferyx.framework.factory.ExecutorFactory;
+import com.inferyx.framework.service.CommonServiceImpl;
+import com.inferyx.framework.writer.IWriter;
+
+/**
+ * @author joy
+ *
+ */
+@Component
+public class PredictMLOperator {
+	
+	@Autowired
+	private SparkContext sparkContext;
+	@Autowired
+	private HDFSInfo hdfsInfo;
+	@Autowired
+	private CommonServiceImpl<?> commonServiceImpl;
+	@Autowired
+	private SQLContext sqlContext;
+	@Autowired
+	private MetadataUtil daoRegister;
+	@Autowired
+	private DataSourceFactory datasourceFactory;
+	@Autowired
+	private ConnectionFactory connectionFactory;
+	@Autowired
+	private ExecutorFactory execFactory;
+	@Autowired
+	private SparkMLOperator sparkMLOperator; 
+
+	/**
+	 * 
+	 */
+	public PredictMLOperator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@SuppressWarnings({ "unchecked", "unused" })
+	public Object predict(Predict predict, Model model, Algorithm algorithm, Datapod targetDp, Dataset<Row> df, String[] fieldArray, TrainExec latestTrainExec,
+			VectorAssembler va, String targetType, String tableName, String filePathUrl, String filePath, String clientContext) throws Exception {
+
+			Vector features = null; // extract feature column as vector from df
+			String modelName = algorithm.getModelName();
+			Class<?> modelClass = Class.forName(modelName);
+
+			MetaIdentifierHolder datastoreHolder = latestTrainExec.getResult();
+			DataStore dataStore = (DataStore) commonServiceImpl.getOneByUuidAndVersion(datastoreHolder.getRef().getUuid(),
+					datastoreHolder.getRef().getVersion(), datastoreHolder.getRef().getType().toString());
+			if (dataStore == null)
+				throw new NullPointerException("No datastore available");
+			String location = dataStore.getLocation();
+
+			if (location.contains("/data"))
+				location = location.replaceAll("/data", "");
+			
+			location = hdfsInfo.getHdfsURL() + location;
+
+			Object trainedModel = modelClass.getMethod("load", String.class).invoke(modelClass, location);
+			Dataset<Row> predictionDf = (Dataset<Row>) trainedModel.getClass().getMethod("transform", Dataset.class)
+					.invoke(trainedModel, df);
+			predictionDf.show();
+
+			String uid = (String) trainedModel.getClass().getMethod("uid").invoke(trainedModel);
+
+			if (targetType.equalsIgnoreCase(MetaType.datapod.toString())) {
+				Datasource datasource = commonServiceImpl.getDatasourceByApp();
+
+				List<Attribute> attributeList = targetDp.getAttributes();
+				Attribute version = new Attribute();
+				version.setActive("Y");
+				version.setAttributeId(attributeList.size());
+				version.setDesc("version");
+				version.setDispName("version");
+				version.setName("version");
+				version.setType("String");
+				version.setPartition("N");
+				attributeList.add(version);
+				targetDp.setAttributes(attributeList);
+
+				df.createOrReplaceGlobalTempView("tempPredictResult");
+				IConnector connector = connectionFactory.getConnector(datasource.getType().toLowerCase());
+				ConnectionHolder conHolder = connector.getConnection();
+				if (conHolder.getStmtObject() instanceof SparkSession) {
+					SparkSession sparkSession = (SparkSession) conHolder.getStmtObject();
+					predictionDf.persist(StorageLevel.MEMORY_AND_DISK());
+					sparkSession.sqlContext().registerDataFrameAsTable(predictionDf, "tempPredictResult");
+				}
+				IExecutor exec = execFactory.getExecutor(datasource.getType());
+				String columns = "";
+				for (String col : predictionDf.columns())
+					columns = columns.concat(col).concat(" AS ").concat(col).concat(",");
+				columns = columns.substring(0, columns.length() - 2);
+				String sql = "SELECT " + columns + " FROM " + "tempPredictResult";
+				ResultSetHolder rsHolder = exec.executeSql(sql, commonServiceImpl.getApp().getUuid());
+
+				Dataset<Row> dfTask = rsHolder.getDataFrame();
+				dfTask.cache();
+
+				sqlContext.registerDataFrameAsTable(dfTask, tableName.replaceAll("-", "_"));
+
+				dfTask.printSchema();
+				IWriter datapodWriter = datasourceFactory.getDatapodWriter(targetDp, daoRegister);
+				datapodWriter.write(dfTask, filePathUrl, targetDp, SaveMode.Append.toString());
+				return filePathUrl;
+			} else {
+				if (sparkMLOperator.save(modelName, trainedModel, sparkContext, filePathUrl))
+					return filePathUrl + "/data";
+				else
+					return null;
+			}	
+	}
+
+
+}
---
+++ framework-service/src/main/java/com/inferyx/framework/operator/TrainAndValidateOperator.java
@@ -0,0 +1,176 @@
+/**
+ * 
+ */
+package com.inferyx.framework.operator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkContext;
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
+import org.apache.spark.ml.Transformer;
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.param.ParamMap;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.dmg.pmml.PMML;
+import org.jpmml.model.MetroJAXBUtil;
+import org.jpmml.sparkml.ConverterUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.inferyx.framework.common.HDFSInfo;
+import com.inferyx.framework.domain.Algorithm;
+import com.inferyx.framework.domain.Model;
+import com.inferyx.framework.domain.Train;
+
+/**
+ * @author joy
+ *
+ */
+@Component
+public class TrainAndValidateOperator {
+	
+	@Autowired
+	private SparkContext sparkContext;
+	@Autowired
+	private HDFSInfo hdfsInfo;
+	@Autowired
+	private SparkMLOperator sparkMLOperator; 
+
+	static final Logger logger = Logger.getLogger(SparkMLOperator.class);
+
+	/**
+	 * 
+	 */
+	public TrainAndValidateOperator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Object trainAndValidate(Train train, Model model, Algorithm algorithm,String modelClassName, String modelName, Dataset<Row> df, VectorAssembler va,
+			ParamMap paramMap, String filePathUrl,String filePath) throws Exception {
+		PipelineModel trngModel = null;
+		List<String> customDirectories = new ArrayList<>();
+		try {
+			Dataset<Row>[] splits = df
+					.randomSplit(new double[] { train.getTrainPercent() / 100, train.getValPercent() / 100 }, 12345);
+			Dataset<Row> trngDf = splits[0];
+			Dataset<Row> valDf = splits[1];
+			Dataset<Row> trainingDf = null;
+			Dataset<Row> validateDf = null;
+			if (algorithm.getTrainName().contains("LinearRegression")
+					|| algorithm.getTrainName().contains("LogisticRegression")) {
+				trainingDf = trngDf.withColumn("label", trngDf.col(model.getLabel()).cast("Double")).select("label",
+						va.getInputCols());
+				validateDf = valDf.withColumn("label", valDf.col(model.getLabel()).cast("Double")).select("label",
+						va.getInputCols());
+			} else {
+				trainingDf = trngDf;
+				validateDf = valDf;
+			}
+
+			Dataset<Row> trainedDataSet = null;
+			@SuppressWarnings("unused")
+			StringIndexer labelIndexer = null;
+			@SuppressWarnings("unused")
+			String labelColName = (modelClassName.contains("classification")) ? "indexedLabel" : "label";
+			/*
+			 * labelIndexer = new StringIndexer() .setInputCol("label")
+			 * .setOutputCol(labelColName);
+			 */
+
+			Class<?> dynamicClass = Class.forName(modelClassName);
+			Object obj = dynamicClass.newInstance();
+			Method method = null;
+			if (algorithm.getTrainName().contains("LinearRegression")
+					|| algorithm.getTrainName().contains("LogisticRegression")) {
+				method = dynamicClass.getMethod("setLabelCol", String.class);
+				method.invoke(obj, "label");
+			}
+
+			method = dynamicClass.getMethod("setFeaturesCol", String.class);
+			method.invoke(obj, "features");
+			Pipeline pipeline = new Pipeline()
+					.setStages(new PipelineStage[] { /* labelIndexer, */va, (PipelineStage) obj });
+			if (null != paramMap) {
+				trngModel = pipeline.fit(trainingDf, paramMap);
+			} else {
+				trngModel = pipeline.fit(trainingDf);
+			}
+
+			// Below line is validation step. Won't create a model. It would create a
+			// DataSet<Row> which can be checked for validation
+			trainedDataSet = trngModel.transform(validateDf);
+			trainedDataSet.show();
+
+			Transformer[] transformers = trngModel.stages();
+			for (int i = 0; i < transformers.length; i++) {
+				customDirectories.add(i + "_" + transformers[i].uid());
+			}
+
+			/*
+			 * List<Row> rowList = trainedDataSet.takeAsList(1); Vector vec = (Vector)
+			 * rowList.get(0).get(4); vec = vec.compressed();
+			 */
+
+			// Vector features = new DenseVector(values)
+			boolean result = sparkMLOperator.save(modelName, trngModel, sparkContext, filePathUrl);
+			if (algorithm.getSavePmml().equalsIgnoreCase("Y")) {
+				try {
+					logger.info("trainedDataSet schema : " + trainedDataSet.schema());
+					if(filePathUrl.contains(hdfsInfo.getHdfsURL()))
+						filePathUrl = filePathUrl.replaceAll(hdfsInfo.getHdfsURL(), "");
+					
+					String fileLocation = filePathUrl + "/" + model.getUuid() + "_" + model.getVersion() + "_"
+							+ (filePathUrl.substring(filePathUrl.lastIndexOf("/") + 1)) + ".pmml";
+					
+					trainedDataSet.show();
+					
+					PMML pmml = ConverterUtil.toPMML(trainedDataSet.schema(), trngModel);
+					MetroJAXBUtil.marshalPMML(pmml, new FileOutputStream(new File(fileLocation), true));					
+				}catch (JAXBException e) {
+					e.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} 
+			}
+			if (result)
+				return filePathUrl + "/stages/" + customDirectories.get(1) + "/data/";
+			else
+				return null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return false;
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+			return false;
+		} catch (SecurityException e) {
+			e.printStackTrace();
+			return false;
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} 
+
+		return null;
+	}
+
+
+}
