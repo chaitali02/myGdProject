56e470c895595d841d70724af485fbac86e6bd67
Rohini1010 <rchaudhari129@gmail.com>
2018-04-21 13:34:42 +0530

    update api

--- framework-service/src/main/java/com/inferyx/framework/service/ModelServiceImpl.java
+++ framework-service/src/main/java/com/inferyx/framework/service/ModelServiceImpl.java
@@ -1374,14 +1374,17 @@ public HttpServletResponse downloadLog(String trainExecUuid, String trainExecVer
 						return true;
 					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ioExp) {
 						ioExp.printStackTrace();
+						return false;
 					} catch (Exception e2) {
 						e2.printStackTrace();
+						return false;
 					}
 				}
 			}
 		} catch (NoSuchMethodException 
 				| SecurityException e) {
 			e.printStackTrace();
+			return false;
 		}
 
 		return false;
