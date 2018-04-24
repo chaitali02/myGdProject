86b72d7286ea8aae78888b2eb283374b737a6d78
sonali1010 <goyaljitender88@gmail.com>
2018-04-24 19:39:31 +0530

    added migration assist import

---
+++ framework-web/src/main/webapp/newapp/src/app/metadata/services/migration-assist.services.ts
@@ -0,0 +1,51 @@
+import { Observable } from 'rxjs/Observable';
+import { Inject, Injectable, Input } from '@angular/core';
+import { Http,Response,Headers } from '@angular/http'
+import { SharedService } from '../../shared/shared.service';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/catch';
+// import 'rxjs/add/operator/promise';
+
+@Injectable()
+export class MigrationAssistService{
+  headers:Headers; 
+  constructor(@Inject(Http) private http: Http,private _sharedService: SharedService) { }
+  
+  exportSubmit(type:any,data:any): Observable<any[]> {
+    let url ='/admin/export/submit?action=view&type='+type;
+    return this._sharedService.postCall(url,data)
+    .map((response: Response) => {
+      return <any>response.text();
+}) 
+   .catch(this.handleError);
+  }
+
+  
+//   upload(type:any,data:any): Observable<any[]> {
+//     let url ='/admin/export/submit?action=view&type='+type;
+//     return this._sharedService.postCall(url,data)
+//     .map((response: Response) => {
+//       return <any>response.text();
+// })
+//    .catch(this.handleError);
+//   }
+
+
+//   uploadFile(fd,filename,type){
+//     var url =this.baseUrl+'metadata/file?action=edit&fileName='+filename+'&type='+type;
+//     let body= fd;
+//     return this.http                 
+//     .post( url,body, {headers: this.headers})
+//   }
+uploadFile(fd,filename,type,fileType){
+  let baseUrl ='http://localhost:8080'
+  var url =baseUrl+'/admin/upload?action=edit&fileName='+filename+'&type='+type+'&fileType='+fileType;
+  let body= fd;
+  return this.http                 
+  .post( url,body, {headers: this.headers})
+}
+
+  private handleError(error: Response) {
+    return Observable.throw(error.statusText);
+}
+}
