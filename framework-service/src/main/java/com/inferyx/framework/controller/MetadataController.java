e565e90d22fbc87fa7d6f957e996da67446e11c8
Rohini1010 <rchaudhari129@gmail.com>
2018-05-14 20:20:19 +0530

    enhace api for max version

--- framework-service/src/main/java/com/inferyx/framework/controller/MetadataController.java
+++ framework-service/src/main/java/com/inferyx/framework/controller/MetadataController.java
@@ -890,8 +890,17 @@ public class MetadataController {
 	
 	@RequestMapping(value = "/getParamListByRule", method = RequestMethod.GET)
 	public @ResponseBody List<ParamList> getParamListByRule(
-			@RequestParam(value = "type", required = false,defaultValue = "rule") String collectionType) throws JsonProcessingException {		
-		return metadataServiceImpl.getParamList(collectionType);
+			@RequestParam(value = "type", required = false,defaultValue = "rule") String collectionType,
+			@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "userName", required = false) String userName,
+			@RequestParam(value = "startDate", required = false) String startDate,
+			@RequestParam(value = "endDate", required = false) String endDate,
+			@RequestParam(value = "tags", required = false) String tags,
+			@RequestParam(value = "active", required = false) String active,
+			@RequestParam(value = "action", required = false) String action,
+			@RequestParam(value = "published", required = false) String published)
+			throws JsonProcessingException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, NullPointerException, java.text.ParseException {		
+		return metadataServiceImpl.getParamList(collectionType, name, userName, startDate, endDate, tags, active, null, null, published);
 	}
 	/*@RequestMapping(value = "/getParamListBySimulate", method = RequestMethod.GET,params = {"simulate"})
 	public @ResponseBody List<ParamList> getParamListBySimulate(	
