b6e7aab2ec7c74ca581c28d9fc3ab5a0b010d3c4
jkumar88 <goyaljitender88@gmail.com>
2018-05-15 19:39:23 +0530

    simulation execution enhanced for unform distributionn on features for join DF

--- framework-service/src/main/java/com/inferyx/framework/executor/SparkExecutor.java
+++ framework-service/src/main/java/com/inferyx/framework/executor/SparkExecutor.java
@@ -19,9 +19,11 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 
@@ -59,6 +61,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.inferyx.framework.common.HDFSInfo;
+import com.inferyx.framework.common.Helper;
 import com.inferyx.framework.common.MetadataUtil;
 import com.inferyx.framework.connector.ConnectionHolder;
 import com.inferyx.framework.connector.IConnector;
@@ -90,6 +93,8 @@ import com.inferyx.framework.service.ParamSetServiceImpl;
 import com.inferyx.framework.writer.IWriter;
 
 import scala.collection.Iterator;
+import scala.collection.JavaConversions;
+import scala.collection.JavaConverters;
 import scala.collection.Seq;
 import scala.collection.mutable.WrappedArray;
 
@@ -1177,13 +1182,26 @@ public class SparkExecutor implements IExecutor {
 	public String assembleRandomDF(String[] fieldArray, String tableName, boolean isDistribution, String clientContext) throws IOException{
 		String sql = "SELECT * FROM " + tableName;
 		Dataset<Row> df = executeSql(sql, clientContext).getDataFrame();
+		
 		if(isDistribution)
 			df = df.withColumnRenamed(df.columns()[1], fieldArray[0]);
 		else
 			fieldArray = df.columns();
+
+		df.printSchema();
+		df.show(true);
+		
 		VectorAssembler va = (new VectorAssembler().setInputCols(fieldArray).setOutputCol("features"));
 		Dataset<Row> assembledDf = va.transform(df);
+		assembledDf.printSchema();
 		assembledDf.show(false);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		tableName = tableName + "_" + Helper.getVersion();
 		sparkSession.sqlContext().registerDataFrameAsTable(assembledDf, tableName);
 		return tableName;
 	}
@@ -1482,4 +1500,30 @@ public class SparkExecutor implements IExecutor {
 		}
 	}
 
+	public String joinDf(String joinTabName_1, String joinTabName_2, int i, String clientContext) throws IOException {
+		String sql_1 = "SELECT * FROM " + joinTabName_1;
+		Dataset<Row> df_1 = executeSql(sql_1, clientContext).getDataFrame();
+		
+		String sql_2 = "SELECT * FROM " + joinTabName_2;
+		Dataset<Row> df_2 = executeSql(sql_2, clientContext).getDataFrame();
+		df_2 = df_2.withColumnRenamed("features", "features_"+i);
+		
+		Set<String> combinedColumns = new HashSet<>();
+		for(String column : df_1.columns()) {
+			combinedColumns.add(column);
+		}
+		combinedColumns.addAll(Arrays.asList(df_1.columns()));
+		combinedColumns.addAll(Arrays.asList(df_2.columns()));
+//		df_1.show(true);
+//		df_2.show(true);
+//		df_1 = df_1.join(df_2, JavaConverters.asScalaBufferConverter(new ArrayList<>(combinedColumns)).asScala(), "full");
+		
+		df_1 = df_1.crossJoin(df_2);
+		df_1.select("id",JavaConverters.asScalaBufferConverter(new ArrayList<>(combinedColumns)).asScala()).show(true);
+		df_1.printSchema();
+		df_1.show(true);
+		
+		registerTempTable(df_1, joinTabName_1);
+		return joinTabName_1;
+	}
 }
--- framework-service/src/main/java/com/inferyx/framework/service/ModelServiceImpl.java
+++ framework-service/src/main/java/com/inferyx/framework/service/ModelServiceImpl.java
@@ -106,6 +106,7 @@ import com.inferyx.framework.executor.ExecContext;
 import com.inferyx.framework.executor.IExecutor;
 import com.inferyx.framework.executor.PythonExecutor;
 import com.inferyx.framework.executor.RExecutor;
+import com.inferyx.framework.executor.SparkExecutor;
 import com.inferyx.framework.factory.ConnectionFactory;
 import com.inferyx.framework.factory.DataSourceFactory;
 import com.inferyx.framework.factory.ExecutorFactory;
@@ -186,6 +187,8 @@ public class ModelServiceImpl {
 	private MonteCarloSimulation monteCarloSimulation;
 	@Autowired
 	private GenerateDataOperator generateDataOperator;
+	@Autowired
+	private SparkExecutor sparkExecutor;
 	
 	//private ParamMap paramMap;
 
@@ -997,16 +1000,31 @@ public class ModelServiceImpl {
 					
 					tableName = generateDataOperator.execute(null, execParams, new MetaIdentifier(MetaType.simulateExec, simulateExec.getUuid(), simulateExec.getVersion()), null, otherParams, null, runMode);
 					
-					//Object object = mlDistribution.getDistribution(distribution, distExecParam);
-						
-					//String tabName_1 = exec.generateFeatureData(object, model.getFeatures(), simulate.getNumIterations(), (tableName+"_"+"form_rand_df"));
-					String tabName_2 = exec.assembleRandomDF(fieldArray, tableName, true, appUuid);
-					String sql = simulateMLOperator.generateSql(simulate, tabName_2);
-					//result = exec.executeAndRegister(sql, tableName, commonServiceImpl.getApp().getUuid());
-					result = exec.executeRegisterAndPersist(sql, tabName_2, filePath, null, SaveMode.Append.toString(), appUuid);					
-				} else if(model.getDependsOn().getRef().getType().equals(MetaType.algorithm)) {
-					//Object object = mlDistribution.getDistribution(distribution, distExecParam);
+					String tabName_2 = null;
+					String tableName_3 = null;
+					if(distribution.getClassName().contains("UniformRealDistribution")) {
+						for(int i=0; i<fieldArray.length; i++) {
+//							tableName = generateDataOperator.execute(null, execParams, new MetaIdentifier(MetaType.simulateExec, simulateExec.getUuid(), simulateExec.getVersion()), null, otherParams, null, runMode);
+							String[] customFldArr = new String[] {fieldArray[i]};
+							tabName_2 = exec.assembleRandomDF(customFldArr, tableName, true, appUuid);
+							String sql = simulateMLOperator.generateSql(simulate, tabName_2);
+							result = exec.executeAndRegister(sql, tabName_2, appUuid);//(sql, tabName_2, filePath, null, SaveMode.Append.toString(), appUuid);
 
+							if(i == 0)
+								tableName_3 = tabName_2;
+							if(i>0)
+								tableName_3 = sparkExecutor.joinDf(tableName_3, tabName_2, i, appUuid);
+						}
+					} else {
+//						tableName = generateDataOperator.execute(null, execParams, new MetaIdentifier(MetaType.simulateExec, simulateExec.getUuid(), simulateExec.getVersion()), null, otherParams, null, runMode);
+						String[] customFldArr = new String[] {fieldArray[0]};
+						tableName_3 = exec.assembleRandomDF(customFldArr, tableName, true, appUuid);
+					}
+					
+					String sql = "SELECT * FROM " + tableName_3;
+					result = exec.executeRegisterAndPersist(sql, tableName_3, filePath, null, SaveMode.Append.toString(), appUuid);					
+				} else if(model.getDependsOn().getRef().getType().equals(MetaType.algorithm)) {
+					
 					HashMap<String, String> otherParams = execParams.getOtherParams();
 					if(otherParams == null)
 						otherParams = new HashMap<>();
@@ -1014,23 +1032,30 @@ public class ModelServiceImpl {
 					
 					tableName = generateDataOperator.execute(null, execParams, new MetaIdentifier(MetaType.simulateExec, simulateExec.getUuid(), simulateExec.getVersion()), null, otherParams, null, runMode);
 					
-					//String tabName_1 = exec.generateFeatureData(object, model.getFeatures(), simulate.getNumIterations(), (tableName+"_"+"algo_rand_df"));
 					String tabName_2 = null;
-//					for(int i=0; i<fieldArray.length; i++) {
+					String tableName_3 = null;
+					if(distribution.getClassName().contains("UniformRealDistribution")) {
+						for(int i=0; i<fieldArray.length; i++) {
+//							tableName = generateDataOperator.execute(null, execParams, new MetaIdentifier(MetaType.simulateExec, simulateExec.getUuid(), simulateExec.getVersion()), null, otherParams, null, runMode);
+							String[] customFldArr = new String[] {fieldArray[i]};
+							tabName_2 = exec.assembleRandomDF(customFldArr, tableName, true, appUuid);
+	
+							if(i == 0)
+								tableName_3 = tabName_2;
+							if(i>0)
+								tableName_3 = sparkExecutor.joinDf(tableName_3, tabName_2, i, appUuid);
+						}
+					} else {
+//						tableName = generateDataOperator.execute(null, execParams, new MetaIdentifier(MetaType.simulateExec, simulateExec.getUuid(), simulateExec.getVersion()), null, otherParams, null, runMode);
 						String[] customFldArr = new String[] {fieldArray[0]};
-						tabName_2 = exec.assembleRandomDF(customFldArr, tableName, true, appUuid);	
-//						tableName = tabName_2;
-//					}
-					
-					String sql = "SELECT * FROM " + tabName_2;
-					//result = exec.executeAndRegister(sql, tableName, commonServiceImpl.getApp().getUuid());
-					result = exec.executeRegisterAndPersist(sql, tabName_2, filePath, null, SaveMode.Append.toString(), appUuid);				
+						tableName_3 = exec.assembleRandomDF(customFldArr, tableName, true, appUuid);
+					}
+					String sql = "SELECT * FROM " + tableName_3;
+					result = exec.executeRegisterAndPersist(sql, tableName_3, filePath, null, SaveMode.Append.toString(), appUuid);				
 				}
 			}
 			
-			
 			dataStoreServiceImpl.setRunMode(runMode);
-
 			dataStoreServiceImpl.create(filePathUrl, modelName,
 					new MetaIdentifier(MetaType.simulate, simulate.getUuid(), simulate.getVersion()),
 					new MetaIdentifier(MetaType.simulateExec, simulateExec.getUuid(), simulateExec.getVersion()),
