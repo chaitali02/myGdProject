ea35394b696c91b8f9c1f22aa4585a944a3daa05
Rohini1010 <neel.shah4593@gmail.com>
2018-05-19 17:12:30 +0530

    added predict in all 3 modes

--- framework-web/src/main/webapp/newapp/src/app/data-science/prediction/prediction.component.ts
+++ framework-web/src/main/webapp/newapp/src/app/data-science/prediction/prediction.component.ts
@@ -18,6 +18,21 @@ import { ResponseOptions } from '@angular/http/src/base_response_options';
   templateUrl: './prediction.template.html',
 })
 export class PredictionComponent implements OnInit {
+  msgs: any[];
+  checkboxModelexecution: boolean;
+  isTargetNameDisabled: boolean;
+  selectTarget: any;
+  allTarget: any[];
+  selectTargetType: string;
+  targetTypes: string[];
+  allTargetAttribute: any[];
+  featureMapTableArray: any[];
+  selectSource: DependsOn;
+  allSource: any[];
+  selectModel: DependsOn;
+  selectSourceType: any;
+  sourceTypes: any[];
+  allModel: any[];
   name: any;
   version: any;
   breadcrumbDataFrom: any;
@@ -34,12 +49,21 @@ export class PredictionComponent implements OnInit {
   isSubmit: any
   selectedVersion: Version;
   VersionList: SelectItem[] = [];
-    constructor(config: AppConfig, private activatedRoute: ActivatedRoute, public router: Router, private _commonService: CommonService, private _location: Location, private _modelService: PredictionService) {
+  
+  
+    constructor(config: AppConfig, private activatedRoute: ActivatedRoute, public router: Router, private _commonService: CommonService, private _location: Location, private _predictService: PredictionService) {
     this.prediction = {};
     this.prediction["active"] = true;
     this.continueCount = 1;
     this.progressbarWidth = 25 * this.continueCount + "%";
-
+    // this.sourceTypes = [{label:"datapod",value:"datapod"},
+    // {label:"dataset",value:"dataset"},
+    // {label:"rule",value:"rule"},
+    // ]
+    this.sourceTypes=["datapod","dataset","rule"]
+    this.selectSourceType=this.sourceTypes[0];
+    this.targetTypes = ["datapod","file"];
+    this.selectTargetType=this.targetTypes[0]; 
     this.breadcrumbDataFrom = [{
       "caption": "Data Science",
       "routeurl": "/app/list/predict"
@@ -65,9 +89,14 @@ export class PredictionComponent implements OnInit {
       if (this.mode !== undefined) {
         this.getOneByUuidAndVersion();
         this.getAllVersionByUuid();
-
       }
-
+      else{
+        this.getAllLatestModel()
+        this.getAllLatestSource(this.selectSourceType)
+        this.getAllLatestTarget(this.selectTargetType)
+        this.getAttribute()
+      }
+     
     })
   }
 
@@ -80,7 +109,74 @@ export class PredictionComponent implements OnInit {
       error => console.log("Error :: " + error));
 
   }
+  getAllLatestModel(){
+    this._predictService.getAllModelByType("N","model")
+    .subscribe(
+    response => {
+      this.onSuccessgetAllLatestModel(response)
+    },
+    error => console.log("Error :: " + error));
+  }
+  onSuccessgetAllLatestModel(response){
+    console.log(response)
+    var temp = []
+    for (const i in response) {
+      let ver = {};
+      ver["label"] = response[i]['name'];
+      ver["value"] = {};
+      ver["value"]["label"] = response[i]['name'];
+      ver["value"]["uuid"] = response[i]['uuid'];
+      ver["value"]["version"] = response[i]['version'];
+      temp[i] = ver;
+    }
+    this.allModel = temp
+  }
+  getAllLatestSource(source){
+    this._commonService.getAllLatest(source)
+    .subscribe(
+    response => {
+      this.onSuccessgetAllLatestSource(response)
+    },
+    error => console.log("Error :: " + error));
+  }
+  getAllLatestTarget(target){
+    this._commonService.getAllLatest(target)
+    .subscribe(
+    response => {
+      this.onSuccessgetAllLatestTarget(response)
+    },
+    error => console.log("Error :: " + error));
+  }
+  onSuccessgetAllLatestTarget(response){
+    var temp = []
+    for (const i in response) {
+      let ver = {};
+      ver["label"] = response[i]['name'];
+      ver["value"] = {};
+      ver["value"]["label"] = response[i]['name'];
+      ver["value"]["uuid"] = response[i]['uuid'];
+      temp[i] = ver;
+    }
+    this.allTarget = temp
+    //this.selectTarget=this.allTarget[0]
+  }
 
+  onChangeSourceType(){
+    this.getAllLatestSource(this.selectSourceType)
+  }
+  onSuccessgetAllLatestSource(response){
+    var temp = []
+    for (const i in response) {
+      let ver = {};
+      ver["label"] = response[i]['name'];
+      ver["value"] = {};
+      ver["value"]["label"] = response[i]['name'];
+      ver["value"]["uuid"] = response[i]['uuid'];
+      temp[i] = ver;
+    }
+    this.allSource = temp
+    //this.selectSource=this.allSource[0]
+  }
   public get value(): string {
     return
   }
@@ -98,7 +194,7 @@ export class PredictionComponent implements OnInit {
   }
 
   countContinue() {
-    this.continueCount = this.continueCount - 1;
+    this.continueCount = this.continueCount + 1;
     this.progressbarWidth = 25 * this.continueCount + "%";
 
   }
@@ -117,7 +213,22 @@ export class PredictionComponent implements OnInit {
       },
       error => console.log("Error :: " + error));
   }
-
+  onChangeActive(event) {
+    if(event === true) {
+      this.prediction.active = 'Y';
+    }
+    else {
+      this.prediction.active = 'N';
+    }
+  }
+  onChangePublish(event) {
+    if(event === true) {
+      this.prediction.published = 'Y';
+    }
+    else {
+      this.prediction.published = 'N';
+    }
+  }
 
   onSuccessgetOneByUuidAndVersion(response) {
     this.prediction = response;
@@ -130,10 +241,78 @@ export class PredictionComponent implements OnInit {
     this.prediction.active = response["active"] == 'Y' ? true : false;
     this.prediction.published = response["published"] == 'Y' ? true : false;
     this.breadcrumbDataFrom[2].caption = response.name;
-    console.log('Data is' + response)
+    let dependOnTemp: DependsOn = new DependsOn();
+    dependOnTemp.label = response["dependsOn"]["ref"]["name"];
+    dependOnTemp.uuid = response["dependsOn"]["ref"]["uuid"];
+    dependOnTemp.version = response["dependsOn"]["ref"]["version"];
+    this.selectModel=dependOnTemp
+    // this.selectSourceType.label=response["source"]["ref"]["type"];
+    // this.selectSourceType.value=response["source"]["ref"]["type"];
+    this.selectSourceType=response["source"]["ref"]["type"];
+    this.selectTargetType=response["target"]["ref"]["type"];
+    let sourceTemp: DependsOn = new DependsOn();
+    sourceTemp.label = response["source"]["ref"]["name"];
+    sourceTemp.uuid = response["source"]["ref"]["uuid"];
+    this.selectSource=sourceTemp
+    let targetTemp: DependsOn = new DependsOn();
+    targetTemp.label = response["target"]["ref"]["name"];
+    targetTemp.uuid = response["target"]["ref"]["uuid"];
+    this.selectTarget=targetTemp
+    this.getAllLatestModel()
+    this.getAllLatestSource(this.selectSourceType)
+    this.getAllLatestTarget(this.selectTargetType)
+    this.getAttribute()
+    var featureMapTableArray=[];
+    for(var i=0;i<response.featureAttrMap.length;i++){
+      var featureMap={};
+      var sourceFeature={};
+      var targetFeature={};
+      featureMap["featureMapId"]=response.featureAttrMap[i].featureMapId;
+      // sourceFeature.datapodname = response.featureMap[i].sourceFeature.ref.name;
+      // sourceFeature.name = response.featureMap[i].sourceFeature.attrName;
+      // sourceFeature.attributeId = response.featureMap[i].sourceFeature.attrId;
+      // sourceFeature.id = response.featureMap[i].sourceFeature.ref.uuid + "_" + response.featureMap[i].sourceFeature.attrId;
+      // sourceFeature.dname = response.featureMap[i].sourceFeature.ref.name + "." + response.featureMap[i].sourceFeature.attrName;
+      sourceFeature["uuid"] = response.featureAttrMap[i].feature.ref.uuid;
+      sourceFeature["type"] = response.featureAttrMap[i].feature.ref.type;
+      sourceFeature["featureId"] = response.featureAttrMap[i].feature.featureId;
+      sourceFeature["featureName"] = response.featureAttrMap[i].feature.featureName;
+      featureMap["sourceFeature"]=sourceFeature;
+      targetFeature["uuid"] = response.featureAttrMap[i].attribute.ref.uuid;
+      targetFeature["type"] = response.featureAttrMap[i].attribute.ref.type;
+      targetFeature["datapodname"] = response.featureAttrMap[i].attribute.ref.name;
+      targetFeature["name"] = response.featureAttrMap[i].attribute.attrName;
+      targetFeature["attributeId"] = response.featureAttrMap[i].attribute.attrId;
+      targetFeature["id"] = response.featureAttrMap[i].attribute.ref.uuid + "_" + response.featureAttrMap[i].attribute.attrId;
+      targetFeature["dname"] = response.featureAttrMap[i].attribute.ref.name + "." + response.featureAttrMap[i].attribute.attrName;
+      featureMap["targetFeature"]=targetFeature;
+      featureMapTableArray[i]=featureMap;
+    }
+    this.featureMapTableArray=featureMapTableArray;
   }
+  onChangeSource(){
+    this.getAttribute()
+  }
+  getAttribute(){
+    this._commonService.getAllAttributeBySource(this.selectSource.uuid,this.selectSourceType).subscribe(
+      response => { 
+        this.OnSuccesgetAllLatest(response)},
+      error => console.log('Error :: ' + error)
+    )
+  }
+  OnSuccesgetAllLatest(response){
+    let temp=[]
+    for (const n in response) {
+      let allname={};
+      allname["label"]=response[n]['dname'];
+      allname["value"]={};
+      allname["value"]["label"]=response[n]['dname'];      
+      allname["value"]["id"]=response[n]['id'];
+      temp[n]=allname;
+    }
+    this.allTargetAttribute = temp
 
-
+  }
   onSuccessgetAllVersionByUuid(response) {
     var temp = []
     for (const i in response) {
@@ -146,8 +325,162 @@ export class PredictionComponent implements OnInit {
     }
     this.VersionList = temp
   }
+  onChangeModel(){
+    // PredictService.getOneByUuidandVersion(this.selectModel.uuid,this.selectModel.version,"model").then(function(response) { onSuccessGetLatestByUuid(response.data)});
+    this._commonService.getOneByUuidAndVersion(this.selectModel.uuid, this.selectModel.version, 'model')
+    .subscribe(
+    response => {
+      this.onSuccessonChangeModel(response)
+    },
+    error => console.log("Error :: " + error));
+  }
+  onSuccessonChangeModel(response) {
+    var featureMapTableArray=[];
+    for(var i=0;i<response.features.length;i++){
+      var featureMap={};
+      var sourceFeature={};
+      var targetFeature={};
+      featureMap["featureMapId"]=i;
+      sourceFeature["uuid"] = response.uuid;
+      sourceFeature["type"] = "model";
+      sourceFeature["featureId"] = response.features[i].featureId;
+      sourceFeature["featureName"] = response.features[i].name;
+      featureMap["sourceFeature"]=sourceFeature;
+      featureMapTableArray[i]=featureMap;
+    }
+    this.featureMapTableArray=featureMapTableArray;
+}
+onChangeTargeType(){
+  if(this.selectTargetType =='datapod'){
+    this.isTargetNameDisabled=false;
+    this.getAllLatestTarget(this.selectTargetType);
+    
+  }else{
+    this.isTargetNameDisabled=true;
+    this.allTarget =[];
+  }
+}
+public goBack() {
+  //this._location.back();
+  this.router.navigate(['app/list/predict']);
+  
+}
+enableEdit(uuid, version) {
+  this.router.navigate(['app/dataScience/prediction',uuid,version, 'false']);
+}
 
+showview(uuid, version) {
+  this.router.navigate(['app/dataScience/prediction',uuid,version, 'true']);
+}
+submit(){
+  var predictJson = {}
+  predictJson["uuid"] = this.prediction.uuid
+  predictJson["name"] = this.prediction.name
+  predictJson["desc"] = this.prediction.desc
+  predictJson["active"] = this.prediction.active == true ?'Y' :"N";
+  predictJson["published"]=this.prediction.published == true ?'Y' :"N"; 
+  // let tagArray=[];
+  // if(this.dqdata.tags !=null){
+  //   for(var counttag=0;counttag<this.dqdata.tags.length;counttag++){
+  //        tagArray[counttag]=this.dqdata.tags[counttag];
+  //   }
+  // }
+  var tagArray = [];
+  if (this.tags != null) {
+    for (var counttag = 0; counttag < this.tags.length; counttag++) {
+      tagArray[counttag] = this.tags[counttag].text;
+    }
+  }
+  predictJson["tags"] = tagArray;
+  var dependsOn={};
+  var ref={};
+  ref["type"]="model";
+  ref["uuid"]=this.selectModel.uuid;
+  dependsOn["ref"]=ref;
+  predictJson["dependsOn"]=dependsOn;
+  var source={};
+  var sourceref={};
+  sourceref["type"]=this.selectSourceType;
+  sourceref["uuid"]=this.selectSource.uuid;
+  source["ref"]=sourceref;
+  predictJson["source"]=source;
+  var target={};
+  var targetref={};
+  targetref["type"]=this.selectTargetType;
+  if(this.selectTargetType =="datapod")
+  targetref["uuid"]=this.selectTarget.uuid;
+  target["ref"]=targetref;
+  predictJson["target"]=target;
+  var featureMap=[];
+  if(this.featureMapTableArray.length >0){
+    for(var i=0;i<this.featureMapTableArray.length;i++){
+      var featureMapObj={};
+      featureMapObj["featureMapId"]=i;
+      var sourceFeature={};
+      var sourceFeatureRef={};
+      var targetFeature={};
+      var targetFeatureRef={};
+      sourceFeatureRef["uuid"] = this.featureMapTableArray[i].sourceFeature.uuid;
+      sourceFeatureRef["type"] = this.featureMapTableArray[i].sourceFeature.type;
+      sourceFeature["ref"]=sourceFeatureRef;
+      //sourceFeature.attrId = this.featureMapTableArray[i].sourceFeature.attributeId;
+      sourceFeature["featureId"] = this.featureMapTableArray[i].sourceFeature.featureId;
+      sourceFeature["featureName"] = this.featureMapTableArray[i].sourceFeature.featureName;
+      featureMapObj["feature"]=sourceFeature;
 
+      let  uuid=this.featureMapTableArray[i].targetFeature.id.split("_")[0]
+      var attrid=this.featureMapTableArray[i].targetFeature.id.split("_")[1]
+      targetFeatureRef["uuid"] = uuid;
+      targetFeatureRef["type"] =this.selectSourceType;
+      targetFeature["ref"]=targetFeatureRef
+      targetFeature["attrId"] =attrid;
+      
+      featureMapObj["attribute"]=targetFeature;
+      featureMap[i]=featureMapObj;
+    }
+  }
+  predictJson["featureAttrMap"]=featureMap;
+  console.log(JSON.stringify(predictJson))
+  this._commonService.submit("predict",predictJson).subscribe(
+    response => { this.OnSuccessubmit(response)},
+    error => console.log('Error :: ' + error)
+  )
+}
+OnSuccessubmit(response){
+  if (this.checkboxModelexecution == true) {
+    this._commonService.getOneById("predict",response).subscribe(
+        response => {this.OnSucessGetOneById(response); },
+        error => console.log('Error :: ' + error)
+    )
+  } //End if
+  else{
+    this.isSubmit="false";
+   // this.IsProgerssShow="false";
+    this.msgs = [];
+    this.msgs.push({severity:'success', summary:'Success Message', detail:'DQ Save Successfully'});
+    setTimeout(() => {
+    this.goBack();
+    
+    }, 1000);
+  }
+}
+OnSucessGetOneById(response){
+  this._commonService.execute(response.uuid,response.version,"predict","execute").subscribe(
+    response => {
+     this.showMassage('Predict Save and Submit Successfully','success','Success Message')
+     setTimeout(() => {
+      this.goBack()
+    }, 1000);
+    },
+    error => console.log('Error :: ' + error)
+  )
+}
+showMassage(msg,msgtype,msgsumary){
+  this.isSubmit="false";
+  //this.IsProgerssShow="false";
+  this.msgs = [];
+  this.msgs.push({severity:msgtype, summary:msgsumary, detail:msg});
+}
 }
 
 
