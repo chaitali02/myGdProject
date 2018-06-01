7839a905fbb1e7da10cba93fea13c514885ccd51
CDevendra <deven.chandode@gmail.com>
2018-06-01 19:06:40 +0530

    add Tag pojo and dao

---
+++ framework-service/src/main/java/com/inferyx/framework/dao/ITagDao.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (C) Inferyx Inc, 2018 All rights reserved. 
+ *
+ * This unpublished material is proprietary to Inferyx Inc.
+ * The methods and techniques described herein are considered  trade 
+ * secrets and/or confidential. Reproduction or distribution, in whole or 
+ * in part, is forbidden.
+ *
+ * Written by Yogesh Palrecha <ypalrecha@inferyx.com>
+ *******************************************************************************/
+package com.inferyx.framework.dao;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import com.inferyx.framework.domain.Tag;
+
+
+public interface ITagDao extends MongoRepository<Tag, String> {
+	@Query(value = "{ 'uuid' : ?0 , 'version' : ?1 }")
+	public Tag findOneByUuidAndVersion(String uuid, String version);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}}, 'uuid' : ?1 , 'version' : ?2 }")
+	public Tag findOneByUuidAndVersion(String appUuid, String uuid, String version);
+
+	@Query(value = "{$group :{ _id : '$uuid', maxVersion : {$max : '$version'} }}")
+	public List<Tag> test(String param1);
+
+	@Query(value = "{'uuid':?0}")
+	public Tag findAllByUuid(String uuid);
+	
+	@Query(value="{'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid':?1}")
+	public Tag findAllByUuid(String appUuid, String uuid);
+
+	@Query(value = "{}")
+	public Tag findLatest(Sort sort);
+	
+	@Query(value="{'appInfo':{$elemMatch: { 'ref.uuid': ?0}}}")
+	public Tag findLatest(String appUuid, Sort sort);
+	
+	@Query(value = "{ 'uuid' : ?0 }")
+	public Tag findLatestByUuid(String uuid, Sort sort);
+	
+	@Query(value="{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}}, 'uuid' : ?1 }")
+	public Tag findLatestByUuid(String appUuid, String datapodUUID, Sort sort);
+
+	@Query(value = "{ 'userInfo.ref.uuid' : ?0 }")
+	public List<Tag> findActivityByUser(String uuid, Sort sort);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid' : ?1}")
+	public List<Tag> findAllVersion(String appUuid, String uuid);
+
+	@Query(value = "{'uuid' : ?0}")
+	public List<Tag> findAllVersion(String uuid);
+	
+	@Query(value="{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}}}")
+	public List<Tag> findAll(String appUuid);	
+	
+	@Query(value="{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}} , '_id' : ?1 }")
+	public Tag findOneById(String appUuid, String id);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid' : ?1, 'version':{$lte:?2 }}")
+	public Tag findAsOf(String appUuid, String uuid, String version, Sort sort);
+
+	@Query(value = "{ 'uuid' : ?0, 'version':{$lte:?1 }}")
+	public Tag findAsOf(String uuid, String version, Sort sort);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'_id' : ?1}")
+	public void delete(String appUuid, String id);
+	
+
+	@Query(value = "{ '_id' : ?0 }")
+	public Tag findOneById(String id);
+	
+	@Query(value="{}")
+	public List<Tag> findAll();	
+	
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'_id' : ?1}")
+	public Tag save(String appUuid, String id);
+	
+	@Query(value = "{'_id' : ?0}")
+	public Tag save(String id);
+
+	@Query(value = "{'_id' : ?0}")
+	public void delete(String id);
+
+}
