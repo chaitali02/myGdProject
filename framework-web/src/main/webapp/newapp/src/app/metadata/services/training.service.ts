1275d2169ebcefdff16e86927e85db8abd8ebf0b
sonali1010 <sonali1010@github.com>
2018-06-14 11:48:51 +0530

    adding training service

--- framework-web/src/main/webapp/newapp/src/app/metadata/services/training.service.ts
+++ framework-web/src/main/webapp/newapp/src/app/metadata/services/training.service.ts
@@ -1,9 +1,9 @@
 import { Observable } from 'rxjs/Observable';
 import { Inject, Injectable, Input } from '@angular/core';
-import { Http,Response } from '@angular/http'
+import { Http, Response } from '@angular/http'
 import { SharedService } from '../../shared/shared.service';
-import {CommonService} from './common.service';
-import {AttributeHolder} from './../../metadata/domain/domain.attributeHolder'
+import { CommonService } from './common.service';
+import { AttributeHolder } from './../../metadata/domain/domain.attributeHolder'
 import { BindingFlags } from '@angular/core/src/view';
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/catch';
@@ -11,18 +11,28 @@ import { BindingFlags } from '@angular/core/src/view';
 
 @Injectable()
 
-export class TrainingService{
-    
-  constructor(@Inject(Http) private http: Http,private _sharedService: SharedService,private _commonService:CommonService) { }
-  getAllModelByType(flag:string,type:String): Observable<any[]> {
-    let url ="model/getAllModelByType?action=view&customFlag=" + flag + "&type=" + type+  "&modelType=algorithm";
+export class TrainingService {
+
+  constructor( @Inject(Http) private http: Http, private _sharedService: SharedService, private _commonService: CommonService) { }
+  getAllModelByType(flag: string, type: String): Observable<any[]> {
+    let url = "model/getAllModelByType?action=view&customFlag=" + flag + "&type=" + type + "&modelType=algorithm";
     return this._sharedService.getCall(url)
-    .map((response: Response) => {
-      return <any[]>response.json();
-  })
-   .catch(this.handleError);
+      .map((response: Response) => {
+        return <any[]>response.json();
+      })
+      .catch(this.handleError);
   }
-private handleError(error: Response) {
+
+  getParamSetByAlgorithm(uuid: Number, version: String) {
+    let url = "metadata/getParamSetByAlgorithm?action=view&algorithmUuid=" + uuid + "&algorithmVersion=null" ;
+    return this._sharedService.getCall(url)
+      .map((response: Response) => {
+        return <any[]>response.json();
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: Response) {
     return Observable.throw(error.statusText);
   }
 
