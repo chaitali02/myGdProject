ec46a2350a7a19e957ca18c9d4ceff00f89ea919
jkumar88 <goyaljitender88@gmail.com>
2018-07-26 20:37:15 +0530

    add paramlist into application

--- framework-service/src/main/java/com/inferyx/framework/domain/Application.java
+++ framework-service/src/main/java/com/inferyx/framework/domain/Application.java
@@ -15,12 +15,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection="application")
 public class Application extends BaseEntity{
 	private MetaIdentifierHolder dataSource;
-
+	private MetaIdentifierHolder paramList;
 	public MetaIdentifierHolder getDataSource() {
 		return dataSource;
 	}
-
+	
 	public void setDataSource(MetaIdentifierHolder dataSource) {
 		this.dataSource = dataSource;
 	}
+	
+	public MetaIdentifierHolder getParamList() {
+		return paramList;
+	}
+
+	public void setParamList(MetaIdentifierHolder paramList) {
+		this.paramList = paramList;
+	}
+
+	
 }
