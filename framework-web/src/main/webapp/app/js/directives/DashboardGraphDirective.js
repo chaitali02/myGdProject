a3f73d327646b98fe4b1f4a2569eb261dd02427b
jkumar88 <goyaljitender88@gmail.com>
2018-08-18 15:38:49 +0530

    fix bug

--- framework-web/src/main/webapp/app/js/directives/DashboardGraphDirective.js
+++ framework-web/src/main/webapp/app/js/directives/DashboardGraphDirective.js
@@ -275,17 +275,36 @@
         }); //End Watch
       } ,//End link
       template: `
+        <!--
         <div style="transform: rotate(90deg);transform-origin: left bottom 0;margin-left: -15px;">
           <div>
            {{column.keys[1].attributeName}}
           </div>
         </div>
-        <div style="margin-top: -20px;">
+        <div   style="margin-top: -20px;">
           <canvas id="heatmap" width="280" height="180"></canvas>
+        </div>
         </div>
         <div style="text-align:center;">
           {{column.keys[0].attributeName}}
         </div>
+        <div style="text-align:center; margin-top:10px;" class="heatmapid">
+        -->
+        <div style="text-align:center;">
+        {{column.keys[0].attributeName}}
+      </div>
+        <div class="row">
+        
+        <div  class="col-md-11">
+          <canvas id="heatmap" width="280" height="180"></canvas>
+        </div>
+        <div class="col-md-1" style="transform: rotate(90deg);transform-origin:left bottom 0;margin-top: -15px;">
+          <div>
+           {{column.keys[1].attributeName}}
+          </div>
+        </div>
+        </div>
+       
         <div style="text-align:center; margin-top:10px;" class="heatmapid">
         </div>
         
