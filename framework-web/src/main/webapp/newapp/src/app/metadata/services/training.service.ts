58d3916d9c3341e0882bc6c47e18002996b09739
chaitali <chaitalidhamande@gmail.com>
2018-08-24 19:20:42 +0530

    write services for training component

--- framework-web/src/main/webapp/newapp/src/app/metadata/services/training.service.ts
+++ framework-web/src/main/webapp/newapp/src/app/metadata/services/training.service.ts
@@ -2,9 +2,9 @@ import { Observable } from 'rxjs/Observable';
 import { Inject, Injectable, Input } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import { SharedService } from '../../shared/shared.service';
-import { CommonService } from './common.service';
-import { AttributeHolder } from './../../metadata/domain/domain.attributeHolder'
-import { BindingFlags } from '@angular/core/src/view';
+//import { CommonService } from './common.service';
+//import { AttributeHolder } from './../../metadata/domain/domain.attributeHolder'
+//import { BindingFlags } from '@angular/core/src/view';
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/catch';
 // import 'rxjs/add/operator/promise';
@@ -13,7 +13,7 @@ import { BindingFlags } from '@angular/core/src/view';
 
 export class TrainingService {
 
-  constructor( @Inject(Http) private http: Http, private _sharedService: SharedService, private _commonService: CommonService) { }
+  constructor( @Inject(Http) private http: Http, private _sharedService: SharedService) { }
   getAllModelByType(flag: string, type: String): Observable<any[]> {
     let url = "model/getAllModelByType?action=view&customFlag=" + flag + "&type=" + type + "&modelType=algorithm";
     return this._sharedService.getCall(url)
@@ -23,8 +23,8 @@ export class TrainingService {
       .catch(this.handleError);
   }
 
-  getParamSetByAlgorithm(uuid: Number, version: String) {
-    let url = "metadata/getParamSetByAlgorithm?action=view&algorithmUuid=" + uuid + "&algorithmVersion=null" ;
+  getParamSetByAlgorithm(uuid: Number, version: String, isHyperParam : any) {
+    let url = "metadata/getParamSetByAlgorithm?action=view&algorithmUuid=" + uuid + "&algorithmVersion=null&isHyperParam=" + isHyperParam ;
     return this._sharedService.getCall(url)
       .map((response: Response) => {
         return <any[]>response.json();
@@ -32,19 +32,30 @@ export class TrainingService {
       .catch(this.handleError);
   }
 
-  private handleError(error: Response) {
-    return Observable.throw(error.statusText);
+  getParamListByAlgorithm(uuid: any, version: any, type:any, isHyperParam:any) {
+    let url = "metadata/getParamListByAlgorithm?action=view&uuid=" + uuid + "&version="+version+"&type="+type+"&isHyperParam="+isHyperParam ;
+    return this._sharedService.getCall(url)
+      .map((response: Response) => {
+        return <any[]>response.json();
+      })
+      .catch(this.handleError);
   }
 
+  getParamByParamList(uuid: any, type:any) : Observable<any>{
+    let url = "metadata/getParamByParamList?action=view&uuid=" + uuid + "&type="+type ;
+    return this._sharedService.getCall(url)
+      .map((response: Response) => {
+        return <any[]>response.json();
+      })
+      .catch(this.handleError);
+  }
 
-
-
-  executeWithParams(type, uuid, version, action){ 
+  executeWithParams(type, uuid, version, data){ 
     let url
-    if(type=='train'){
+    if(type=='train'){debugger
       url = "model/train/execute?uuid=" + uuid + "&version=" + version+ '&action=view';
       let body=null
-      return this._sharedService.postCall(url,body)
+      return this._sharedService.postCall(url,data)
       .map((response: Response) => {
         return <any[]>response.json();
     })
@@ -52,5 +63,8 @@ export class TrainingService {
     }
   }
   
+  private handleError(error: Response) {
+    return Observable.throw(error.statusText);
+  }
 
 }
