7f1c21e1925ead8ef3170abb26b530259459b300
CDevendra <deven.chandode@gmail.com>
2018-09-07 22:24:26 +0530

    remove commented method

--- framework-service/src/main/java/com/inferyx/framework/executor/SparkExecutor.java
+++ framework-service/src/main/java/com/inferyx/framework/executor/SparkExecutor.java
@@ -898,16 +898,6 @@ public class SparkExecutor<T> implements IExecutor {
 
 		return data;
 	}
-	/*public static boolean hasColumn(ResultSet rs, String columnName) throws SQLException {
-	    ResultSetMetaData rsmd = rs.getMetaData();
-	    int columns = rsmd.getColumnCount();
-	    for (int x = 1; x <= columns; x++) {
-	        if (columnName.equals(rsmd.getColumnName(x))) {
-	            return true;
-	        }
-	    }
-	    return false;
-	}*/
 
 	@Override
 	public long loadAndRegister(Load load, String filePath, String dagExecVer, String loadExecVer,
