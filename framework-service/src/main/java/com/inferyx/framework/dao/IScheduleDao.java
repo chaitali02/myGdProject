a6b06e219943537e70b494726944216d99fac2a3
CDevendra <deven.chandode@gmail.com>
2018-09-07 12:36:52 +0530

    add schedule privileges and dao

---
+++ framework-service/src/main/java/com/inferyx/framework/dao/IScheduleDao.java
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * Copyright (C) Inferyx Inc, 2018 All rights reserved. 
+ *
+ * This unpublished material is proprietary to Inferyx Inc.
+ * The methods and techniques described herein are considered  trade 
+ * secrets and/or confidential. Reproduction or distribution, in whole or 
+ * in part, is forbidden.
+ *
+ * Written by Yogesh Palrecha <ypalrecha@inferyx.com>
+ *******************************************************************************/
+package com.inferyx.framework.dao;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import com.inferyx.framework.domain.Schedule;
+
+public interface IScheduleDao extends MongoRepository<Schedule, String> {
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid' : ?1 , 'version' : ?2 }")
+	public Schedule findOneByUuidAndVersion(String appUuid, String uuid, String version);
+
+	@Query(value = "{ 'uuid' : ?0 , 'version' : ?1 }")
+	public Schedule findOneByUuidAndVersion(String uuid, String version);
+
+	@Query(value = "{$group :{ _id : '$uuid', maxVersion : {$max : '$version'} }}")
+	public List<Schedule> test(String param1);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}}}")
+	public List<Schedule> findAll(String appUuid);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid':?1}")
+	public List<Schedule> findAllByUuid(String appUuid, String uuid);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid' : ?1 }")
+	public Schedule findLatestByUuid(String appUuid, String uuid, Sort sort);
+
+	@Query(value = "{ 'uuid' : ?0 }")
+	public Schedule findLatestByUuid(String uuid, Sort sort);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid' : ?1}")
+	public List<Schedule> findAllVersion(String appUuid, String uuid);
+
+	@Query(value = "{'uuid' : ?0}")
+	public List<Schedule> findAllVersion(String uuid);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}} , '_id' : ?1 }")
+	public Schedule findOneById(String appUuid, String id);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'_id' : ?1}")
+	public void delete(String appUuid, String id);
+
+	@Query(value = "{'_id' : ?0}")
+	public void delete(String id);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid' : ?1, 'version':{$lte:?2 }}")
+	public Schedule findAsOf(String appUuid, String uuid, String version, Sort sort);
+
+	@Query(value = "{ 'uuid' : ?ScheduleScheduleSchedule0, 'version':{$lte:?1 }}")
+	public Schedule findAsOf(String uuid, String version, Sort sort);
+
+	@Query(value = "{}")
+	public Schedule findLatest(Sort sort);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}}}")
+	public Schedule findLatest(String appUuid, Sort sort);
+
+	@Query(value = "{ '_id' : ?0 }")
+	public Schedule findOneById(String id);
+
+	@Query(value = "{'uuid':?0}")
+	public Schedule findAllByUuid(String uuid);
+
+	@Query(value = "{}")
+	public List<Schedule> findAll();
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'_id' : ?1}")
+	public Schedule save(String appUuid, String id);
+
+	@Query(value = "{'_id' : ?0}")
+	public Schedule save(String id);
+}
