cd880055ce2b440a689f9dfad71c6bb07c2632f1
CDevendra <deven.chandode@gmail.com>
2018-09-07 16:57:46 +0530

    update error msg limit exceed.

--- framework-service/src/main/java/com/inferyx/framework/service/VizpodServiceImpl.java
+++ framework-service/src/main/java/com/inferyx/framework/service/VizpodServiceImpl.java
@@ -1272,9 +1272,9 @@ public class VizpodServiceImpl {
 
 		int maxRows = Integer.parseInt(Helper.getPropertyValue("framework.download.maxrows"));
 		if(rowLimit > maxRows) {
-			logger.error("Number of rows "+rowLimit+" exceeded. Max row allow "+maxRows);
-			commonServiceImpl.sendResponse("412", MessageStatus.FAIL.toString(), "Number of rows "+rowLimit+" exceeded. Max row allow "+maxRows);
-			throw new RuntimeException("Number of rows "+rowLimit+" exceeded. Max row allow "+maxRows);
+			logger.error("Requested rows exceeded the limit of "+maxRows);
+			commonServiceImpl.sendResponse("412", MessageStatus.FAIL.toString(), "Requested rows exceeded the limit of "+maxRows);
+			throw new RuntimeException("Requested rows exceeded the limit of "+maxRows);
 		}
 		
 		VizpodResultHolder resultHolder = getVizpodResults(uuid, version, execParams, rowLimit, offset, limit, sortBy, order, requestId, runMode);
