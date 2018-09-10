6e5a237a5aa00b8f35b7cf872283607436748958
joy <joy.jishnu.10@gmail.com>
2018-09-11 04:36:39 +0530

    Sqoop Connector and Executor

--- framework-service/src/main/java/com/inferyx/framework/connector/SqoopConnector.java
+++ framework-service/src/main/java/com/inferyx/framework/connector/SqoopConnector.java
@@ -5,10 +5,17 @@ package com.inferyx.framework.connector;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Component;
+
+import com.cloudera.sqoop.SqoopOptions;
+import com.inferyx.framework.domain.Datasource;
+import com.inferyx.framework.executor.ExecContext;
+
 /**
  * @author joy
  *
  */
+@Component
 public class SqoopConnector implements IConnector {
 
 	/**
@@ -20,7 +27,63 @@ public class SqoopConnector implements IConnector {
 	
 	@Override
 	public ConnectionHolder getConnection(Object input) throws IOException {
-		// TODO Auto-generated method stub
+		SqoopOptions sqoopOptions = new SqoopOptions();
+		ConnectionHolder connHolder = new ConnectionHolder();
+		Datasource datasource = null;
+		String connectionString = null;
+		String userName = null;
+		String password = null;
+		connHolder.setType(ExecContext.SQOOP.toString());
+		if (input == null) {
+			connHolder.setConObject(sqoopOptions);
+			return connHolder;
+		}
+		if (input instanceof Datasource) {
+			datasource = (Datasource) input;
+			switch (datasource.getType()) {
+			case "ORACLE" : 
+				connectionString = "jdbc:oracle:thin:@" + datasource.getHost() + ":"
+					+ datasource.getPort() + ":" + datasource.getSid();
+				userName = datasource.getUsername();
+				password = datasource.getPassword();
+				break;
+			case "MYSQL" : 
+				connectionString = "jdbc:mysql://" + datasource.getHost() + ":" + datasource.getPort()
+				+ "/" + datasource.getDbname();
+				userName = datasource.getUsername();
+				password = datasource.getPassword();
+				break;
+			case "IMPALA" : 
+				connectionString = "jdbc:postgresql://" + datasource.getHost() + ":" + datasource.getPort()
+				+ "/" + datasource.getDbname();
+				userName = datasource.getUsername();
+				password = datasource.getPassword();
+				break;
+			case "HIVE" : 
+				connectionString = "jdbc:hive2://" + datasource.getHost() + ":" + datasource.getPort()
+				+ "/" + datasource.getDbname();
+				userName = datasource.getUsername();
+				password = datasource.getPassword();
+				break;
+			case "POSTGRES" : 
+				connectionString = "jdbc:postgresql://" + datasource.getHost() + ":" + datasource.getPort()
+				+ "/" + datasource.getDbname();
+				userName = datasource.getUsername();
+				password = datasource.getPassword();
+				break;
+			case "spark" : 
+			case "PYTHON" : 
+			case "R" : 
+			case "SQOOP" : 
+				default : 
+			}
+			sqoopOptions.setConnectString(connectionString);
+		    sqoopOptions.setUsername(userName);
+		    sqoopOptions.setPassword(password);
+		    connHolder.setConObject(sqoopOptions);
+		    return connHolder;
+		}
+			
 		return null;
 	}
 
