19cac94828b64458cf7ec09a7cff35d60edc2df8
Ganesh Sonawane <ganeshsonawane06@hotmail.com>
2018-09-21 11:49:47 +0530

    removed unused imports

--- framework-service/src/main/java/com/inferyx/framework/domain/DataQualGroupExec.java
+++ framework-service/src/main/java/com/inferyx/framework/domain/DataQualGroupExec.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package com.inferyx.framework.domain;
 
-import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
--- framework-service/src/main/java/com/inferyx/framework/service/RuleGroupExecServiceImpl.java
+++ framework-service/src/main/java/com/inferyx/framework/service/RuleGroupExecServiceImpl.java
@@ -10,41 +10,20 @@
  *******************************************************************************/
 package com.inferyx.framework.service;
 
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
-
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.FutureTask;
 
-import javax.annotation.Resource;
-
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.inferyx.framework.common.Helper;
 import com.inferyx.framework.common.MetadataUtil;
 import com.inferyx.framework.dao.IRuleGroupExecDao;
-import com.inferyx.framework.domain.DataQualExec;
-import com.inferyx.framework.domain.DataQualGroupExec;
 import com.inferyx.framework.domain.MetaIdentifier;
-import com.inferyx.framework.domain.MetaIdentifierHolder;
 import com.inferyx.framework.domain.MetaType;
-import com.inferyx.framework.domain.RuleExec;
-import com.inferyx.framework.domain.RuleGroup;
 import com.inferyx.framework.domain.RuleGroupExec;
-import com.inferyx.framework.domain.Status;
 import com.inferyx.framework.domain.User;
 
 @Service
