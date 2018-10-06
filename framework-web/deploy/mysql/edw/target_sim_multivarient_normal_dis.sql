905b9f562eb52639dd0e646ec47febf21d71c9a0
ypalrecha <ypalrecha@gridedge.com>
2018-10-06 12:10:12 -0400

    Fixed all mysql issues

--- framework-web/deploy/mysql/edw/target_sim_multivarient_normal_dis.sql
+++ framework-web/deploy/mysql/edw/target_sim_multivarient_normal_dis.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS `target_sim_multivarient_normal_dis`;
-CREATE TABLE `target_sim_multivarient_normal_dis` (
-  `id` int(11) DEFAULT NULL,
-  `interestRate` double precision,
-  `col2` double precision,
-  `col3` double precision,
-  `version` int(11) DEFAULT NULL
+DROP TABLE IF EXISTS target_sim_multivarient_normal_dis;
+CREATE TABLE target_sim_multivarient_normal_dis (
+  id int(11) DEFAULT NULL,
+  interestRate double precision,
+  col2 double precision,
+  col3 double precision,
+  version int(11) DEFAULT NULL
 );
