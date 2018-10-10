768c1a8c1e01204653d844a23f520754e4d434ca
vaibhavakatkar <vaibhavakatkar@gmail.com>
2018-10-10 17:42:55 +0530

    rm table ecocap

--- framework-web/deploy/oracle/ecocap/customer_portfolio_ul_calc_final_cust_summary.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "customer_portfolio_ul_calc_final_cust_summary" (
-  "cust_id" VARCHAR2(70 BYTE) DEFAULT NULL,
-  "portfolio_ul_cust_summary" NUMBER(20,2) DEFAULT NULL,
-  "reporting_date" VARCHAR2(70 BYTE) DEFAULT NULL,
-  "version" NUMBER(30,0) DEFAULT NULL
-);
-
