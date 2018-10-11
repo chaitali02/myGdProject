c680246649279d8bb76cc321341b9c6a1bfa1e4c
CDevendra <deven.chandode@gmail.com>
2018-10-11 12:13:00 +0530

    update run_all postgres

--- framework-web/deploy/postgres/edw/run_all.sh
+++ framework-web/deploy/postgres/edw/run_all.sh
@@ -1,4 +1,4 @@
-#*******************************************************************************
+ #*******************************************************************************
 # Copyright (C) GridEdge Consulting LLC, 2016 All rights reserved.
 #
 # This unpublished material is proprietary to GridEdge Consulting LLC.
@@ -15,27 +15,29 @@ if [[ $dbname = "" ]] ; then
         exit 1
 fi
 #rm -r run_all.hql
-#rm -r create_all.sql 
+rm create_all.sql 
 cp load.sql load_bck.sql
 cp counts.sql counts_bck.sql
-for file in *.hql
+
+for file in *.sql
 do
         if [[ $file != "load_small.sql" && $file != "load_medium.hql" && $file != "create_db_user.sql" && $file != "counts.sql" && $file != "load.sql" ]] ; then
-			echo >> create_all.sql
-            cat $file >> create_all.sql
-			#sed -i 's/DROP TABLE IF EXISTS /DROP TABLE IF EXISTS '$1'./g' create_all_new.sql 
-			#sed -i 's/CREATE TABLE /CREATE TABLE '$1'./g' create_all_new.sql
-			
-			#sed -i 's/Copy /Copy '$1'./g' load.sql
+        #echo >> create_all.sql
+            cat $file >> create_all.sql            
+			 
         fi;
 done
+#cp create_all.sql create_all_bck.sql
+sed -i 's/DROP TABLE IF EXISTS /DROP TABLE IF EXISTS '$1'./g' create_all_bck.sql
+sed -i 's/ACCOUNT( /ACCOUNT/g' create_all_bck.sql
+sed -i 's/CREATE TABLE /CREATE TABLE '$1'./g' create_all_bck.sql
 sed -i 's/TRUNCATE TABLE /TRUNCATE TABLE '$1'./g' load_bck.sql
 sed -i 's/from /from '$1'./g' counts_bck.sql
-			
-#psql -U postgres -c \ $1 
-#psql -U postgres -d $1 < create_all_new.sql
-#psql -U postgres -d $1 < load_bck.sql
-#psql -U postgres -d $1 < counts_bck.sql
-rm -r load_bck.sql
-rm -r counts_bck.sql
+sed -i 's/Copy /Copy '$1'./g' load_bck.sql
+		 
+psql -U postgres -d $1 < create_all_bck.sql
+psql -U postgres -d $1 < load_bck.sql
+psql -U postgres -d $1 < counts_bck.sql
+#rm load_bck.sql
+#rm counts_bck.sql
 
