a37daa3e124ff8daa9134b6f9caee7e4bb6adebe
jkumar88 <goyaljitender88@gmail.com>
2018-10-11 16:18:24 +0530

    update tostring method

--- framework-service/src/main/java/com/inferyx/framework/domain/SqoopInput.java
+++ framework-service/src/main/java/com/inferyx/framework/domain/SqoopInput.java
@@ -511,9 +511,6 @@ public class SqoopInput {
 		this.hCatalogTableName = hCatalogTableName;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
 		return "SqoopInput [table=" + table + ", sourceDs=" + sourceDs + ", targetDs=" + targetDs + ", sourceDirectory="
@@ -527,6 +524,26 @@ public class SqoopInput {
 				+ ", explicitOutputDelims=" + explicitOutputDelims + ", importIntended=" + importIntended
 				+ ", fileLayout=" + fileLayout + ", overwriteHiveTable=" + overwriteHiveTable + ", hiveTableName="
 				+ hiveTableName + ", hiveDatabaseName=" + hiveDatabaseName + ", hCatalogTableName=" + hCatalogTableName
-				+ ", hCatalogDatabaseName=" + hCatalogDatabaseName + ", sqlQuery=" + sqlQuery + "]";
-	}	
+				+ ", hCatalogDatabaseName=" + hCatalogDatabaseName + ", sqlQuery=" + sqlQuery + ", splitByCol="
+				+ splitByCol + "]";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+//	@Override
+//	public String toString() {
+//		return "SqoopInput [table=" + table + ", sourceDs=" + sourceDs + ", targetDs=" + targetDs + ", sourceDirectory="
+//				+ sourceDirectory + ", targetDirectory=" + targetDirectory + ", hiveImport=" + hiveImport
+//				+ ", partitionKey=" + partitionKey + ", partitionValue=" + partitionValue + ", whereClause="
+//				+ whereClause + ", compressionCodec=" + compressionCodec + ", checkColumn=" + checkColumn
+//				+ ", lastVale=" + lastVale + ", appendMode=" + appendMode + ", incrementalMode=" + incrementalMode
+//				+ ", incrementalTestColumn=" + incrementalTestColumn + ", incrementalLastValue=" + incrementalLastValue
+//				+ ", exportDir=" + exportDir + ", numMappers=" + numMappers + ", linesTerminatedBy=" + linesTerminatedBy
+//				+ ", fieldsTerminatedBy=" + fieldsTerminatedBy + ", explicitInputDelims=" + explicitInputDelims
+//				+ ", explicitOutputDelims=" + explicitOutputDelims + ", importIntended=" + importIntended
+//				+ ", fileLayout=" + fileLayout + ", overwriteHiveTable=" + overwriteHiveTable + ", hiveTableName="
+//				+ hiveTableName + ", hiveDatabaseName=" + hiveDatabaseName + ", hCatalogTableName=" + hCatalogTableName
+//				+ ", hCatalogDatabaseName=" + hCatalogDatabaseName + ", sqlQuery=" + sqlQuery + "]";
+//	}	
 }
