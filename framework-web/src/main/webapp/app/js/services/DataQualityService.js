23d5ee9c141ddaab9d07ae6772ed5350e7ab39a5
jkumar88 <goyaljitender88@gmail.com>
2018-10-22 18:16:40 +0530

    fix filter detail page

--- framework-web/src/main/webapp/app/js/services/DataQualityService.js
+++ framework-web/src/main/webapp/app/js/services/DataQualityService.js
@@ -689,6 +689,7 @@ DataQualityModule.service("DataqulityService", function ($q, DataQualityFactory,
 							filterInfo.rhsvalue2 = response.filter.filterInfo[i].operand[1].value.split("and")[1];
 						} else if (['<', '>', "<=", '>='].indexOf(response.filter.filterInfo[i].operator) != -1) {
 							obj.caption = "integer";
+							filterInfo.rhsvalue = response.filterInfo[i].operand[1].value
 
 						} else if (response.filter.filterInfo[i].operator == '=' && response.filter.filterInfo[i].operand[1].attributeType =="integer") {
 							obj.caption = "integer";
