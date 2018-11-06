3d49aadd31d40c22435a1f6c996f48466d2a67a4
jkumar88 <goyaljitender88@gmail.com>
2018-11-06 14:11:52 +0530

    enhance row up and down for dataset

--- framework-web/src/main/webapp/app/js/controllers/DatasetController.js
+++ framework-web/src/main/webapp/app/js/controllers/DatasetController.js
@@ -1017,12 +1017,12 @@ MetadataModule.controller('MetadataDatasetController', function (dagMetaDataServ
 			
 			if (res != -1) {
 				$scope.isDuplication = true;
-				$scope.myform3.$dirty=true;
+			//	$scope.myform3.$dirty=true;
 				
 			
 			} else {
 				$scope.isDuplication = false;
-				$scope.myform3.$dirty=false;
+			//	$scope.myform3.$dirty=false;
 				
 			}
 		}
@@ -1269,7 +1269,7 @@ MetadataModule.controller('MetadataDatasetController', function (dagMetaDataServ
 		for (var l = 0; l < $scope.attributeTableArray.length; l++) {
 			attributeinfo = {}
 			attributeinfo.attrSourceId =$scope.attributeTableArray[l].id;
-			//attributeinfo.attrSourceId = l;
+			attributeinfo.attrDisplaySeq = l;
 			attributeinfo.attrSourceName = $scope.attributeTableArray[l].name
 			var ref = {};
 			var sourceAttr = {};
@@ -1360,7 +1360,10 @@ MetadataModule.controller('MetadataDatasetController', function (dagMetaDataServ
 
 		}
 	}
-
+    $scope.ondrop = function(e) {
+		console.log(e);
+		$scope.myform3.$dirty=true;
+	  }
 	$scope.expandAll = function (expanded) {
 		// $scope is required here, hence the injection above, even though we're using "controller as" syntax
 		$scope.$broadcast('onExpandAll', { expanded: expanded });
@@ -1407,4 +1410,134 @@ MetadataModule.controller('MetadataDatasetController', function (dagMetaDataServ
   	        }]
   	    };
   	});*/
+	
+	  MetadataModule.directive("dragDrop", ["$parse",
+	  function($parse) {
+		var sourceParent = "";
+		var sourceIndex = -1;
+		return {
+		  link: function($scope, elm, attr, ctrl) {
+	
+			// #region Initialization
+	
+			// Get TBODY of a element  
+			var tbody = elm.parent();
+			// Set draggable true
+			//elm.attr("draggable", true);jitender
+
+			// If id of TBODY of current element already set then it won't set again
+			tbody.attr('drag-id') ? void 0 : tbody.attr("drag-id", $scope.$id);
+			// This add drag pointer 
+			elm.css("cursor", "move");
+	
+			// Events of element :- dragstart | dragover | drop | dragend
+			elm.on("dragstart", onDragStart);
+			elm.on("dragover", onDragOver);
+			elm.on("drop", onDrop);
+			elm.on("dragend", onDragEnd);
+	
+			// #endregion
+	
+			// This will trigger when user pick e row
+			function onDragStart(e) {
+	
+			  //Mozilla Hack
+		//	  e.dataTransfer.setData("Text", "");
+	
+			  if (!sourceParent) {
+	
+				// Set selected element's parent id
+				sourceParent = tbody.attr('drag-id') ? tbody.attr('drag-id') : void 0;
+	
+				// Set selected element's index
+				sourceIndex = $scope.$index;
+	
+				// This don't support in IE but other browser support it
+				// This will set drag Image with it's position
+				// IE automically set image by himself
+				// typeof e.dataTransfer.setDragImage !== "undefined" ?
+				//   e.dataTransfer.setDragImage(e.target, -10, -10) : void 0;
+	
+				// This element will only drop to the element whose have drop effect 'move'
+			//	e.dataTransfer.effectAllowed = 'move';
+			  }
+			  return true;
+			}
+	
+			// This will trigger when user drag source element on another element
+			function onDragOver(e) {
+	
+			  // Prevent Default actions
+			  e.preventDefault ? e.preventDefault() : void 0;
+			  e.stopPropagation ? e.stopPropagation() : void 0;
+	
+			  // This get current elements parent id
+			  var targetParent = tbody.attr('drag-id') ? tbody.attr('drag-id') : void 0;
+	
+	
+			  // If user drag elemnt from its boundary then cursor will show block icon else it will show move icon [ i.e : this effect work perfectly in google chrome]
+			//  e.dataTransfer.dropEffect = sourceParent !== targetParent || typeof attr.ngRepeat === "undefined" ? 'none' : 'move';
+	
+			  return false;
+			}
+	
+			//This will Trigger when user drop source element on target element
+			function onDrop(e) {
+	
+			  // Prevent Default actions
+			  e.preventDefault ? e.preventDefault() : void 0;
+			  e.stopPropagation ? e.stopPropagation() : void 0;
+	
+			  if (typeof attr.ngRepeat === "undefined")
+				return false;
+			  // Get this item List
+			  var itemList = $parse(attr.ngRepeat.split("in")[1].trim())($scope);
+	
+	
+			  // Get target element's index
+			  var targetIndex = $scope.$index;
+	
+			  // Get target element's parent id
+			  var targetParent = tbody.attr('drag-id') ? tbody.attr('drag-id') : void 0;
+	
+			  // Get properties names which will be changed during the drag and drop
+			  var elements = attr.dragDrop ? attr.dragDrop.trim().split(",") : void 0;
+	
+			  // If user dropped element into it's boundary and on another source not himself
+			  if (sourceIndex !== targetIndex && targetParent === sourceParent) {
+	
+				// If user provide element list by ',' 
+				typeof elements !== "undefined" ? elements.forEach(function(element) {
+				  element = element.trim();
+				  typeof itemList[targetIndex][element] !== "undefined" ?
+					itemList[targetIndex][element] = [itemList[sourceIndex][element], itemList[sourceIndex][element] = itemList[targetIndex][element]][0] : void 0;
+				}) : void 0;
+				// Visual row change 
+				itemList[targetIndex] = [itemList[sourceIndex], itemList[sourceIndex] = itemList[targetIndex]][0];
+				// After completing the task directive send changes to the controller 
+				$scope.$apply(function() {
+				  typeof attr.afterDrop != "undefined" ?
+					$parse(attr.afterDrop)($scope)({
+					  sourceIndex: sourceIndex,
+					  sourceItem: itemList[sourceIndex],
+					  targetIndex: targetIndex,
+					  targetItem: itemList[targetIndex]
+					}) : void 0;
+	
+				});
+			  }
+			}
+			// This will trigger after drag and drop complete
+			function onDragEnd(e) {
+	
+			  //clearing the source
+			  sourceParent = "";
+			  sourceIndex = -1;
+			}
+	
+		  }
+		}
+	  }
+	]);
+	
 	
