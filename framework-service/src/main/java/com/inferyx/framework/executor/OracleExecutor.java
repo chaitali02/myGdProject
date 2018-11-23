64b8ed57810951c26e92504a82ea90ef1ce04905
Ganesh Sonawane <ganeshsonawane06@hotmail.com>
2018-11-23 20:46:10 +0530

    enhace comparemetadata for oracle

--- framework-service/src/main/java/com/inferyx/framework/executor/OracleExecutor.java
+++ framework-service/src/main/java/com/inferyx/framework/executor/OracleExecutor.java
@@ -560,7 +560,7 @@ public class OracleExecutor implements IExecutor {
 		String attrLength = attribute.getLength() != null ? attribute.getLength().toString() : "";
 		if(attribute.getName().equalsIgnoreCase(sourceAttrDetails.get("COLUMN_NAME"))) {	
 			String status = null;			
-			if(sourceAttrDetails.get("TYPE_NAME").toLowerCase().contains(attribute.getType().toLowerCase())) {
+			if(sourceAttrDetails.get("TYPE_NAME").toLowerCase().equalsIgnoreCase(attribute.getType().toLowerCase())) {
 				status = Compare.NOCHANGE.toString();
 			} else {
 				status = Compare.MODIFIED.toString();
