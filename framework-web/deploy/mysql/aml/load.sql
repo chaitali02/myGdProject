7720adcaf65d8f388e0a22bea4822e6c59eabf5d
Ganesh Sonawane <ganeshsonawane06@hotmail.com>
2018-11-26 16:35:03 +0530

    commit

--- framework-web/deploy/mysql/aml/load.sql
+++ framework-web/deploy/mysql/aml/load.sql
@@ -2,12 +2,12 @@ TRUNCATE TABLE fact_transaction_journal;
 LOAD DATA LOCAL INFILE '/user/hive/warehouse/framework/app/aml/data/csv/noheader/fact_transaction_journal.csv' IGNORE INTO TABLE fact_transaction_journal FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r' ;
 
 TRUNCATE TABLE customer_alert_summary;
-LOAD DATA LOCAL INFILE '/user/hive/warehouse/framework/app/aml/data/csv/header/customer_alert_summary.csv' IGNORE INTO TABLE customer_alert_summary FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r' ;
+LOAD DATA LOCAL INFILE '/user/hive/warehouse/framework/app/aml/data/csv/noheader/customer_alert_summary.csv' IGNORE INTO TABLE customer_alert_summary FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r' ;
 
 
 TRUNCATE TABLE rule_alert_summary;
-LOAD DATA LOCAL INFILE '/user/hive/warehouse/framework/app/aml/data/csv/header/rule_alert_summary.csv' IGNORE INTO TABLE rule_alert_summary FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r' ;
+LOAD DATA LOCAL INFILE '/user/hive/warehouse/framework/app/aml/data/csv/noheader/rule_alert_summary.csv' IGNORE INTO TABLE rule_alert_summary FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r' ;
 
 TRUNCATE TABLE country_alert_summary;
-LOAD DATA LOCAL INFILE '/user/hive/warehouse/framework/app/aml/data/csv/header/country_alert_summary.csv' IGNORE INTO TABLE country_alert_summary FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r' ;
+LOAD DATA LOCAL INFILE '/user/hive/warehouse/framework/app/aml/data/csv/noheader/country_alert_summary.csv' IGNORE INTO TABLE country_alert_summary FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r' ;
 
