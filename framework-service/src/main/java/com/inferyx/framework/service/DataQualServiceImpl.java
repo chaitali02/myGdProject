5db33698da09b2b41c7f90a2bbe19c24389a66d6
vaibhavakatkar <vaibhavakatkar@gmail.com>
2018-11-29 17:45:02 +0530

    fix bugs

--- framework-service/src/main/java/com/inferyx/framework/service/DataQualServiceImpl.java
+++ framework-service/src/main/java/com/inferyx/framework/service/DataQualServiceImpl.java
@@ -588,10 +588,10 @@ public class DataQualServiceImpl  extends RuleTemplate{
 		return parse(baseExec.getUuid(), baseExec.getVersion(), DagExecUtil.convertRefKeyListToMap(execParams.getRefKeyList()), execParams.getOtherParams(), null, null, runMode);
 	}
 
-	@Override
+/*	@Override
 	public Datasource getDatasource(BaseRule baseRule) throws JsonProcessingException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, NullPointerException, ParseException {
 		MetaIdentifier datapodRef = ((DataQual)baseRule).getDependsOn().getRef();
 		Datapod datapod = (Datapod) commonServiceImpl.getOneByUuidAndVersion(datapodRef.getUuid(), datapodRef.getVersion(), datapodRef.getType().toString());
 		return commonServiceImpl.getDatasourceByDatapod(datapod);
-	}
+	}*/
 }
