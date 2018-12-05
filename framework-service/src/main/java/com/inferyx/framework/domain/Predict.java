8b8bedcee9a4b80a9f480f08980586159595a8f9
vaibhavakatkar <vaibhavakatkar@gmail.com>
2018-12-05 11:21:34 +0530

    new properties rowIdentifier, includeFeatures

--- framework-service/src/main/java/com/inferyx/framework/domain/Predict.java
+++ framework-service/src/main/java/com/inferyx/framework/domain/Predict.java
@@ -26,6 +26,48 @@ public class Predict extends BaseEntity {
 	private List<FeatureAttrMap> featureAttrMap;
 	private AttributeRefHolder labelInfo;
 	private MetaIdentifierHolder trainInfo;
+	private List<AttributeRefHolder> rowIdentifier;
+	private String includeFeatures = "N";
+
+	/**
+	 *
+	 * @Ganesh
+	 *
+	 * @return the rowIdentifier
+	 */
+	public List<AttributeRefHolder> getRowIdentifier() {
+		return this.rowIdentifier;
+	}
+
+	/**
+	 *
+	 * @Ganesh
+	 *
+	 * @param rowIdentifier the rowIdentifier to set
+	 */
+	public void setRowIdentifier(List<AttributeRefHolder> rowIdentifier) {
+		this.rowIdentifier = rowIdentifier;
+	}
+
+	/**
+	 *
+	 * @Ganesh
+	 *
+	 * @return the includeFeatures
+	 */
+	public String getIncludeFeatures() {
+		return this.includeFeatures;
+	}
+
+	/**
+	 *
+	 * @Ganesh
+	 *
+	 * @param includeFeatures the includeFeatures to set
+	 */
+	public void setIncludeFeatures(String includeFeatures) {
+		this.includeFeatures = includeFeatures;
+	}
 
 	/**
 	 * @Ganesh
