ff5cb1467faa42d3ab324efcc3b1c63026c055a7
joy <joy.jishnu.10@gmail.com>
2018-12-15 02:16:58 +0530

    Predict from trainexec

--- framework-predict/src/main/java/com/inferyx/controller/PredictController.java
+++ framework-predict/src/main/java/com/inferyx/controller/PredictController.java
@@ -4,6 +4,7 @@
 package com.inferyx.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,8 @@ import com.inferyx.framework.domain.MetaIdentifierHolder;
 import com.inferyx.framework.domain.MetaType;
 import com.inferyx.framework.domain.Predict;
 import com.inferyx.framework.domain.PredictExec;
+import com.inferyx.framework.domain.Train;
+import com.inferyx.framework.domain.TrainExec;
 import com.inferyx.framework.domain.User;
 import com.inferyx.framework.enums.RunMode;
 import com.inferyx.framework.service.CommonServiceImpl;
@@ -74,6 +77,71 @@ public class PredictController {
 		}
 		return true;
 	}
+	
+	@RequestMapping(value = "/sample", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String helloController() {
+		return " Hello Controller!!! ";
+	}
+	
+	@RequestMapping(value = "/train/execute", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean predictFromTrain(@RequestParam("uuid") String trainExecUUID, @RequestParam("version") String trainExecVersion,
+			@RequestParam("sourceType") String sourceType,@RequestParam(value = "sourceUUID", required = false) String sourceUUID, 
+			@RequestParam(value = "sourceVersion", required = false) String sourceVersion, 
+			@RequestParam("userId") String userUuid,
+			@RequestParam("appId") String appId,
+			@RequestParam("targetType") String targetType,@RequestParam(value = "targetUUID", required = false) String targetUUID, 
+			@RequestParam(value = "targetVersion", required = false) String targetVersion, 
+			@RequestBody(required = false) ExecParams execParams,
+			@RequestParam(value = "type", required = false) String type,
+			@RequestParam(value = "action", required = false) String action,
+			@RequestParam(value = "mode", required = false, defaultValue = "BATCH") String mode) throws Exception {
+		
+		User user = (User) commonServiceImpl.getOneByUuidAndVersion(userUuid, null, MetaType.user.toString());
+		Application app = (Application) commonServiceImpl.getOneByUuidAndVersion(appId, null, MetaType.application.toString());
+		MetaIdentifier appRef = new MetaIdentifier(MetaType.application, appId, app.getVersion());
+		MetaIdentifierHolder appInfo = new MetaIdentifierHolder(appRef);
+		frameworkThreadServiceImpl.setSession(user.getName(), appInfo);
+		
+		try {
+
+			RunMode runMode = Helper.getExecutionMode(mode);
+			TrainExec trainExec = (TrainExec) commonServiceImpl.getOneByUuidAndVersion(trainExecUUID, trainExecVersion,
+					MetaType.trainExec.toString());
+			Train train = (Train) commonServiceImpl.getOneByUuidAndVersion(trainExec.getDependsOn().getRef().getUuid(), 
+					trainExec.getDependsOn().getRef().getVersion(),
+					MetaType.train.toString());
+			
+			// Create Predict object
+			MetaIdentifier trainInfoRef = new MetaIdentifier(MetaType.train, train.getUuid(), train.getVersion(), train.getName());
+			MetaIdentifierHolder trainInfo = new MetaIdentifierHolder(trainInfoRef);
+			MetaIdentifier sourceIdentifier = new MetaIdentifier(Helper.getMetaType(sourceType), sourceUUID, sourceVersion);
+			MetaIdentifierHolder sourceIdHolder = new MetaIdentifierHolder(sourceIdentifier);
+			MetaIdentifier targetIdentifier = new MetaIdentifier(Helper.getMetaType(targetType), targetUUID, targetVersion);
+			MetaIdentifierHolder targetIdHolder = new MetaIdentifierHolder(targetIdentifier);
+			
+			Predict predict = new Predict();
+			predict.setBaseEntity();
+			predict.setTrainInfo(trainInfo);
+			predict.setFeatureAttrMap(train.getFeatureAttrMap());
+			predict.setIncludeFeatures(train.getIncludeFeatures());
+			predict.setSource(sourceIdHolder);
+			predict.setTarget(targetIdHolder);
+			predict.setDependsOn(train.getDependsOn());
+			predict.setAppInfo(train.getAppInfo());
+			predict.setCreatedBy(train.getCreatedBy());
+//			Predict predict = (Predict) commonServiceImpl.getOneByUuidAndVersion(predictUUID, predictVersion,
+//					MetaType.predict.toString());
+
+			PredictExec predictExec = null;
+			predictExec = modelServiceImpl.create(predict, execParams, null, predictExec);
+			modelServiceImpl.predict(predict, execParams, predictExec, runMode);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
 	
 
 }
