446cac5ea33ad14701c361137b2d70f01e0b690b
vaibhavakatkar <vaibhavakatkar@gmail.com>
2018-12-21 12:04:51 +0530

    added app PredictiveM for testing

--- framework-web/scripts/run.sh
+++ framework-web/scripts/run.sh
@@ -4,7 +4,7 @@
 ./install_application.sh localhost append ../app/ecocap/meta
 ./install_application.sh localhost append ../app/aml/meta
 ./install_application.sh localhost append ../app/dot/meta
-
+./install_application.sh localhost append ../app/predictivem/meta
 ./update_createdOn.sh localhost
 rm -r /user/hive/warehouse/framework/*
 mkdir -p /user/hive/warehouse/framework/data/
