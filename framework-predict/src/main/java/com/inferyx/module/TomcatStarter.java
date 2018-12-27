321d0e279adcac081d4675cd1b454d4f17c93ca6
joy <joy.jishnu.10@gmail.com>
2018-12-27 08:44:42 +0530

    Change in tomcat logger

--- framework-predict/src/main/java/com/inferyx/module/TomcatStarter.java
+++ framework-predict/src/main/java/com/inferyx/module/TomcatStarter.java
@@ -3,6 +3,7 @@ package com.inferyx.module;
 import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.Executor;
+import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
 public class TomcatStarter {
+	
+	static Logger logger = Logger.getLogger(TomcatStarter.class.getName());
 	
 	static String portNumber = null;
 	public TomcatStarter() {
@@ -29,7 +35,7 @@ public class TomcatStarter {
 		tomcat.setPort(Integer.parseInt(args[0]));
 
 		Context ctx = tomcat.addContext("/", new File(".").getAbsolutePath());
-		System.out.println("new File(\".\").getAbsolutePath() : " + new File(".").getAbsolutePath());
+		logger.info("new File(\".\").getAbsolutePath() : " + new File(".").getAbsolutePath());
 
 		XmlWebApplicationContext applicationContext = new XmlWebApplicationContext();
 		applicationContext.setConfigLocations("classpath:framework-predict.xml");
@@ -45,7 +51,32 @@ public class TomcatStarter {
 		      }
 		    });
 		    ctx.addServletMapping("/hello", "hello");
-
+		    
+		    LifecycleListener[] listeners = tomcat.getService().findLifecycleListeners();
+		    if (listeners != null) {
+		    	logger.info(" Number of default listeners : " + listeners.length);
+			    for (LifecycleListener listener : listeners) {
+			    	logger.info(" Listener : " + listener.getClass().getName());
+			    }
+		    }
+		    logger.info(" Proxy port : " + tomcat.getConnector().getProxyPort());
+		    logger.info(" Redirect port : " + tomcat.getConnector().getRedirectPort());
+		    Connector[] connectors = tomcat.getService().findConnectors();
+		    if (connectors != null) {
+		    	logger.info(" Number of default connectors : " + connectors.length);
+			    for (Connector connector : connectors) {
+			    	logger.info(" Connector : " + connector.getClass().getName() + " in port : " + connector.getPort());
+			    }
+		    }
+		    
+		    Executor[] executors = tomcat.getService().findExecutors();
+		    if (connectors != null) {
+		    	logger.info(" Number of default executors : " + executors.length);
+			    for (Executor executor : executors) {
+			    	logger.info(" Executor : " + executor.getClass().getName());
+			    }
+		    }
+		    
 		tomcat.start();
 		tomcat.getServer().await();
 	}
