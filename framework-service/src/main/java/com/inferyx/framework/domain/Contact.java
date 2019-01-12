66261c6d1cd7b77159b49fc51ffefa94b2555084
vaibhavakatkar <vaibhavakatkar@gmail.com>
2019-01-12 12:47:37 +0530

    Added Organization classes

---
+++ framework-service/src/main/java/com/inferyx/framework/domain/Contact.java
@@ -0,0 +1,38 @@
+package com.inferyx.framework.domain;
+
+public class Contact {
+	
+	private String title;
+	private String name;
+	private int phone;
+	private String emailid;
+	
+	public String getTitle() {
+		return this.title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return this.phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getEmailid() {
+		return this.emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	
+	
+	
+
+}
---
+++ framework-service/src/main/java/com/inferyx/framework/domain/Email.java
@@ -0,0 +1,22 @@
+package com.inferyx.framework.domain;
+
+public class Email {
+	
+	String type;
+	String emailid;
+	public String getType() {
+		return this.type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEmailid() {
+		return this.emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	
+	
+
+}
