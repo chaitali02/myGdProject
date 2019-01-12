59dfa7f0b6ce0045f7b8e10b4bd76928c9215e35
ganeshsonawane06 <ganeshm.sonawane06@gmail.com>
2019-01-12 12:42:16 +0530

    removed property EncodingType

--- framework-service/src/main/java/com/inferyx/framework/domain/Predict.java
+++ framework-service/src/main/java/com/inferyx/framework/domain/Predict.java
@@ -30,25 +30,6 @@ public class Predict extends BaseEntity {
 	private MetaIdentifierHolder trainInfo;
 	private List<AttributeRefHolder> rowIdentifier;
 	private String includeFeatures = "N";
-	private EncodingType encodingType;
-
-	/**
-	 * @Ganesh
-	 *
-	 * @return the encodingType
-	 */
-	public EncodingType getEncodingType() {
-		return encodingType;
-	}
-
-	/**
-	 * @Ganesh
-	 *
-	 * @param encodingType the encodingType to set
-	 */
-	public void setEncodingType(EncodingType encodingType) {
-		this.encodingType = encodingType;
-	}
 
 	/**
 	 *
