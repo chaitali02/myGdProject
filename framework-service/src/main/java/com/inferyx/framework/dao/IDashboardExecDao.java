2f87f3d83abdf4653e5929d1c411818d375ec04f
ganeshsonawane06 <ganeshm.sonawane06@gmail.com>
2019-02-13 19:33:23 +0530

    new domain classes

---
+++ framework-service/src/main/java/com/inferyx/framework/dao/IDashboardExecDao.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (C) Inferyx Inc, 2018 All rights reserved. 
+ *
+ * This unpublished material is proprietary to Inferyx Inc.
+ * The methods and techniques described herein are considered  trade 
+ * secrets and/or confidential. Reproduction or distribution, in whole or 
+ * in part, is forbidden.
+ *
+ * Written by Yogesh Palrecha <ypalrecha@inferyx.com>
+ *******************************************************************************/
+package com.inferyx.framework.dao;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import com.inferyx.framework.domain.DashboardExec;
+
+/**
+ * @author Ganesh
+ *
+ */
+public interface IDashboardExecDao extends MongoRepository<DashboardExec, String> {
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'uuid' : ?1 , 'version' : ?2 }")
+	public DashboardExec findOneByUuidAndVersion(String appUuid, String uuid, String version);
+
+	@Query(value = "{'uuid' : ?0 , 'version' : ?1 }")
+	public DashboardExec findOneByUuidAndVersion(String uuid, String version);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'name' : ?1 }")
+	public DashboardExec findOneByFileName(String appUuid, String fileName);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}} , '_id' : ?1 }")
+	public DashboardExec findOneById(String appUuid, String id);
+
+	@Query(value = "{ '_id' : ?0 }")
+	public DashboardExec findOneById(String id);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'id' : ?1, 'attributes.name' : ?2 ,'attributes.type' : ?3 ,'attributes.desc' : ?4}")
+	public List<DashboardExec> findOneForDelete(String appUuid, String id, String name, String type, String desc);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'uuid' : ?1 }")
+	public DashboardExec findLatestByUuid(String appUuid, String uuid, Sort sort);
+
+	@Query(value = "{'uuid' : ?0 }")
+	public DashboardExec findLatestByUuid(String uuid, Sort sort);
+
+	@Query(value = "{$group :{ _id : '$uuid', maxVersion : {$max : '$version'} }}")
+	public List<DashboardExec> test(String param1);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}}}")
+	public List<DashboardExec> findAll(String appUuid);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'uuid':?1}")
+	public DashboardExec findAllByUuid(String appUuid, String uuid);
+
+	@Query(value = "{'uuid':?0}")
+	public DashboardExec findAllByUuid(String uuid);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid' : ?1 }")
+	public List<DashboardExec> findAllVersion(String appUuid, String uuid);
+
+	@Query(value = "{'uuid' : ?0 }")
+	public List<DashboardExec> findAllVersion(String uuid);
+
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'_id' : ?1}")
+	public void delete(String appUuid, String id);
+	
+	@Query(value = "{'_id' : ?0}")
+	public void delete(String id);
+
+	@Query(value = "{ 'appInfo':{$elemMatch: { 'ref.uuid': ?0}},'uuid' : ?1, 'version':{$lte:?2 }}")
+	public DashboardExec findAsOf(String appUuid, String uuid, String version, Sort sort);
+
+	@Query(value = "{ 'uuid' : ?0, 'version':{$lte:?1 }}")
+	public DashboardExec findAsOf(String uuid, String version, Sort sort);
+	
+	@Query(value = "{}")
+	public DashboardExec findLatest(Sort sort);
+	
+	@Query(value="{'appInfo':{$elemMatch: { 'ref.uuid': ?0}}}")
+	public DashboardExec findLatest(String appUuid, Sort sort);
+	
+	@Query(value="{}")
+	public List<DashboardExec> findAll();		
+	
+	@Query(value = "{'appInfo':{$elemMatch: { 'ref.uuid': ?0}} ,'_id' : ?1}")
+	public DashboardExec save(String appUuid, String id);
+	
+	@Query(value = "{'_id' : ?0}")
+	public DashboardExec save(String id);	
+
+}
---
+++ framework-service/src/main/java/com/inferyx/framework/domain/DashboardExec.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (C) Inferyx Inc, 2018 All rights reserved. 
+ *
+ * This unpublished material is proprietary to Inferyx Inc.
+ * The methods and techniques described herein are considered  trade 
+ * secrets and/or confidential. Reproduction or distribution, in whole or 
+ * in part, is forbidden.
+ *
+ * Written by Yogesh Palrecha <ypalrecha@inferyx.com>
+ *******************************************************************************/
+package com.inferyx.framework.domain;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author Ganesh
+ *
+ */
+@Document(collection = "dashboardexec")
+public class DashboardExec extends BaseExec {
+	private List<MetaIdentifierHolder> vizExecInfo;
+
+	/**
+	 * @Ganesh
+	 *
+	 * @return the vizExecInfo
+	 */
+	public List<MetaIdentifierHolder> getVizExecInfo() {
+		return vizExecInfo;
+	}
+
+	/**
+	 * @Ganesh
+	 *
+	 * @param vizExecInfo the vizExecInfo to set
+	 */
+	public void setVizExecInfo(List<MetaIdentifierHolder> vizExecInfo) {
+		this.vizExecInfo = vizExecInfo;
+	}
+	
+}
