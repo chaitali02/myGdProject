667042c66b3d183cfbfce54205c361a8420e9e93
Vaibhav <vaibhavakatkar@gmail.com>
2019-02-15 12:08:41 +0530

    colMatrix

--- framework-service/src/main/java/com/inferyx/framework/executor/SparkExecutor.java
+++ framework-service/src/main/java/com/inferyx/framework/executor/SparkExecutor.java
@@ -62,6 +62,7 @@ import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.linalg.Vector;
 import org.apache.spark.ml.linalg.VectorUDT;
+import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.ml.param.BooleanParam;
 import org.apache.spark.ml.param.DoubleParam;
 import org.apache.spark.ml.param.FloatParam;
@@ -70,6 +71,7 @@ import org.apache.spark.ml.param.LongParam;
 import org.apache.spark.ml.param.Param;
 import org.apache.spark.ml.param.ParamMap;
 import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
+import org.apache.spark.ml.stat.Correlation;
 import org.apache.spark.ml.tuning.CrossValidator;
 import org.apache.spark.ml.tuning.CrossValidatorModel;
 import org.apache.spark.ml.tuning.ParamGridBuilder;
@@ -107,6 +109,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.inferyx.framework.common.Engine;
 import com.inferyx.framework.common.HDFSInfo;
@@ -119,7 +122,6 @@ import com.inferyx.framework.domain.Attribute;
 import com.inferyx.framework.domain.AttributeRefHolder;
 import com.inferyx.framework.domain.BaseExec;
 import com.inferyx.framework.domain.CompareMetaData;
-import com.inferyx.framework.domain.DataSet;
 import com.inferyx.framework.domain.DataStore;
 import com.inferyx.framework.domain.Datapod;
 import com.inferyx.framework.domain.Datasource;
@@ -4261,10 +4263,32 @@ public class SparkExecutor<T> implements IExecutor {
 	
 	public ResultSetHolder corelationMatrix(ResultSetHolder rsHolder) throws IOException {
 		Dataset<Row> df = rsHolder.getDataFrame();	
+		String[] a=df.columns();
+		int s=a.length;
+		df.head(2);
+		df.show();
+		IConnector connector = connectionFactory.getConnector(ExecContext.spark.toString());
+		ConnectionHolder conHolder = connector.getConnection();
+		SparkSession sparkSession = (SparkSession) conHolder.getStmtObject();
+		List<Row> data = Arrays.asList(
+				  RowFactory.create(Vectors.sparse(a.length, new int[]{0, a.length-1}, new double[]{1.0, -2.0})),
+				  RowFactory.create(Vectors.dense(4.0, 5.0, 0.0)),
+				  RowFactory.create(Vectors.dense(6.0, 7.0, 0.0)),
+				  RowFactory.create(Vectors.sparse(a.length, new int[]{0, a.length-1}, new double[]{9.0, 1.0}))
+				);
 
+				StructType schema = new StructType(new StructField[]{
+				  new StructField("features", new VectorUDT(), false, Metadata.empty()),
+				});
+
+			Dataset<Row> df1 = sparkSession.createDataFrame(data, schema);
+				Row r1 = Correlation.corr(df1, "features").head();
+				System.out.println("Pearson correlation matrix:\n" + r1.get(1).toString());
+				df1.printSchema();
+				df1.show();
+				df.show();
+		rsHolder.setDataFrame(df1);
 		
-		
-		rsHolder.setDataFrame(df);
 		return rsHolder;
 	}
 }
