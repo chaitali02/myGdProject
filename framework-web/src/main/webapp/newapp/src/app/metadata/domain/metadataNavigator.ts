710a85b4530084cc7bf8f7e9b2c66c33cfbeb474
chaitali <chaitalidhamande@gmail.com>
2019-02-18 20:11:47 +0530

    updated POJOs

---
+++ framework-web/src/main/webapp/newapp/src/app/metadata/domain/metadataNavigator.ts
@@ -0,0 +1,9 @@
+export class MetadataNavigator {
+  count: String;
+  lastUpdatedBy: String;
+  lastUpdatedOn: String;
+  type: String;
+  colorClass: String;//for color
+  icon: String;
+  caption: String;
+}
---
+++ framework-web/src/main/webapp/newapp/src/app/metadata/domainIO/domain.metadataIO.ts
@@ -0,0 +1,24 @@
+export class MetadataIO{
+    name : String;
+    caption: String;
+    execType: String;
+    metaType: String;
+    color: String;
+    icon?: String;
+    iconPath: String;
+    iconCaption?:String;
+    allowInMenu: Boolean;
+    detailState: String;
+    listState: String;
+    resultState?: String;
+    moduleState: String;
+    moduleCaption: String;
+    class: String;
+    iconPathInactive?: String;
+    state?: String
+    childMenu?: any;
+    allowInChildMenu?: Boolean;
+}
+
+    //state: 'createdataqualitygroup',
+//}
