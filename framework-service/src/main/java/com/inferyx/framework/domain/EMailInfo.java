8b8f909696865afbef835090464549cbd5eddd7d
ganeshsonawane06 <ganeshm.sonawane06@gmail.com>
2019-02-26 16:03:16 +0530

    emailInfo -notification pojo changes

---
+++ framework-service/src/main/java/com/inferyx/framework/domain/EMailInfo.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright (C) Inferyx Inc, 2018 All rights reserved. 
+ *
+ * This unpublished material is proprietary to Inferyx Inc.
+ * The methods and techniques described herein are considered  trade 
+ * secrets and/or confidential. Reproduction or distribution, in whole or 
+ * in part, is forbidden.
+ *
+ * Written by Yogesh Palrecha <ypalrecha@inferyx.com>
+ *******************************************************************************/
+package com.inferyx.framework.domain;
+
+import java.util.List;
+
+/**
+ * @author Ganesh
+ *
+ */
+public class EMailInfo {
+	private List<String> emailTo;
+	private List<String> emailBCC;
+	private List<String> emailCC;
+	private List<String> emailAttachment;
+	
+	/**
+	 * @Ganesh
+	 *
+	 * @return the emailTo
+	 */
+	public List<String> getEmailTo() {
+		return emailTo;
+	}
+	/**
+	 * @Ganesh
+	 *
+	 * @param emailTo the emailTo to set
+	 */
+	public void setEmailTo(List<String> emailTo) {
+		this.emailTo = emailTo;
+	}
+	/**
+	 * @Ganesh
+	 *
+	 * @return the emailBCC
+	 */
+	public List<String> getEmailBCC() {
+		return emailBCC;
+	}
+	/**
+	 * @Ganesh
+	 *
+	 * @param emailBCC the emailBCC to set
+	 */
+	public void setEmailBCC(List<String> emailBCC) {
+		this.emailBCC = emailBCC;
+	}
+	/**
+	 * @Ganesh
+	 *
+	 * @return the emailCC
+	 */
+	public List<String> getEmailCC() {
+		return emailCC;
+	}
+	/**
+	 * @Ganesh
+	 *
+	 * @param emailCC the emailCC to set
+	 */
+	public void setEmailCC(List<String> emailCC) {
+		this.emailCC = emailCC;
+	}
+	/**
+	 * @Ganesh
+	 *
+	 * @return the emailAttachment
+	 */
+	public List<String> getEmailAttachment() {
+		return emailAttachment;
+	}
+	/**
+	 * @Ganesh
+	 *
+	 * @param emailAttachment the emailAttachment to set
+	 */
+	public void setEmailAttachment(List<String> emailAttachment) {
+		this.emailAttachment = emailAttachment;
+	}
+}
