eec6d2600153bbf272f77b36fa3cf245579d5510
Vaibhav <vaibhavakatkar@gmail.com>
2019-03-10 16:41:46 +0530

    mysql schema update for aml

--- framework-web/deploy/mysql/aml/counts.sql
+++ framework-web/deploy/mysql/aml/counts.sql
@@ -1,5 +1,11 @@
 select tabname,cnt from
 (
+select 'rule_result_detail' as tabname, count(*) as cnt from aml_small.rule_result_detail
+union
+select 'rule_result_summary' as tabname, count(*) as cnt from aml_small.rule_result_summary
+union
+select 'account_summary_daily' as tabname, count(*) as cnt from aml_small.account_summary_daily
+union
 select 'fact_transaction_journal' as tabname, count(*) as cnt from aml_small.fact_transaction_journal
 union
 select 'customer_alert_summary' as tabname, count(*) as cnt from aml_small.customer_alert_summary
--- framework-web/deploy/mysql/aml/load.sql
+++ framework-web/deploy/mysql/aml/load.sql
@@ -1,3 +1,6 @@
+truncate table account_summary_daily;
+load data local infile '/user/hive/warehouse/framework/app/aml/data/csv/noheader/account_summary_daily.csv' ignore into table account_summary_daily fields terminated by ',' enclosed by '"' escaped by '"' lines terminated by '\n' ;
+
 truncate table fact_transaction_journal;
 load data local infile '/user/hive/warehouse/framework/app/aml/data/csv/noheader/fact_transaction_journal2.csv' ignore into table fact_transaction_journal fields terminated by ',' enclosed by '"' escaped by '"' lines terminated by '\n' ;
 
