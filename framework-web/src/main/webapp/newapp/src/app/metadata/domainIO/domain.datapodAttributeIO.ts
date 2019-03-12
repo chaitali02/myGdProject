ea9194b4484c2721703e2091b8c286da9edbc90b
chaitali <chaitalidhamande@gmail.com>
2019-03-12 20:33:41 +0530

    added POJOs

---
+++ framework-web/src/main/webapp/newapp/src/app/metadata/domainIO/domain.datapodAttributeIO.ts
@@ -0,0 +1,16 @@
+export class DatapodAttributeIO{
+    attributeId : any;
+    displaySeq : any;
+    name : String;
+    type : String;
+    desc : String;
+    expression : String;
+    key : any;
+    partition : any;
+    dispName : String;
+    active : any;
+    length : any;
+    attrUnitType : any;
+    isAttributeEnable?: Boolean;
+    selected? :Boolean;
+}
