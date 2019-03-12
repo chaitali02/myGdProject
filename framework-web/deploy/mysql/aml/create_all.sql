04425931443109218723d8f1f53cd11bf2e6aa4c
Vaibhav <vaibhavakatkar@gmail.com>
2019-03-12 13:46:40 +0530

    update mysql schema for dq

--- framework-web/deploy/mysql/aml/create_all.sql
+++ framework-web/deploy/mysql/aml/create_all.sql
@@ -145,6 +145,107 @@ create table dim_country(
 	load_date varchar(10),
 	load_id integer(50));
 
+drop table if exists dq_rule_detail;
+ create table dq_rule_detail (
+rule_uuid varchar(50),
+rule_version int(10) ,
+rule_name varchar(50),
+datapod_uuid varchar(50),
+datapod_version int(10) ,
+datapod_name varchar(50),
+attribute_id varchar(50),
+attribute_name varchar(50),
+attribute_value varchar(500),
+rowkey_name varchar(500),
+rowkey_value varchar(500),
+null_check_pass varchar(1) ,
+value_check_pass varchar(1) ,
+range_check_pass varchar(1) ,
+datatype_check_pass varchar(1) ,
+format_check_pass varchar(1) ,
+length_check_pass varchar(1) ,
+ri_check_pass varchar(1) ,
+dup_check_pass varchar(1) ,
+custom_check_pass varchar(1) ,
+domain_check_pass varchar(1) ,
+blankspace_check_pass varchar(1) ,
+expression_check_pass varchar(1) ,
+version int(10));
+
+
+ 
+
+drop table if exists dq_rule_summary;
+ create table dq_rule_summary (
+    rule_uuid        varchar(50),
+ rule_version     int(10),
+ rule_name        varchar(50),
+ datapod_uuid     varchar(50),
+ datapod_version  int(10),
+ datapod_name     varchar(50),
+ all_check_pass   varchar(1),
+ total_row_count  int(10),
+ total_pass_count int(10),
+ total_fail_count int(10),
+ threshold_type   varchar(50),
+ threshold_limit  int(3),
+ threshold_ind    varchar(5),
+ score            int(3),
+ version          int(10));
+
+
+drop table if exists equity_executions;
+ create table equity_executions(
+execution_id int(10),
+version_id int(10),
+execution_event_type varchar(50),
+order_id  int(10),
+execution_date varchar(50),
+execution_time varchar(50),
+execution_event_date varchar(50),
+execution_event_time varchar(50),
+execution_price double,
+trading_account_id int(10),
+position_trader_id int(10),
+security_id int(10),
+security_symbol varchar(50),
+ric_code  varchar(50),
+security_description varchar(50),
+client_account_id int(10),
+parent_order_id int(10),
+quantity double,
+reason varchar(50),
+version  int(10));
+
+
+drop table if exists equity_orders;
+ create table equity_orders(
+order_id int(10),
+version_id int(10),
+order_event_type varchar(50),
+order_placement_date varchar(50),
+order_placement_time varchar(50),
+order_event_date varchar(50),
+order_event_time varchar(50),
+time_in_force varchar(50),
+order_type varchar(50),
+limit_price double,
+stop_price double,
+trading_account_id int(10),
+sales_trader_id  int(10),
+position_trader_id int(10),
+security_id int(10),
+security_symbol  varchar(50),
+ric_code varchar(50),
+security_description varchar(50),
+client_account_id int(10),
+parent_order_id  int(10),
+route_destination varchar(50),
+quantity double,
+reason varchar(50),
+version  int(10));
+
+
 drop table if exists fact_transaction_journal;
 
 create table fact_transaction_journal 
@@ -200,6 +301,7 @@ drop table if exists rule_result_detail;
 create table rule_result_detail
 (
  rule_uuid varchar(50),
+  rule_version varchar(50),
   rule_name varchar(50),
   entity_type varchar(50),
   entity_id varchar(50),
@@ -208,7 +310,6 @@ create table rule_result_detail
   criteria_met_ind varchar(50),
   criteria_expr varchar(50),
   criteria_score  decimal(12,1),
-  rule_version varchar(50),
   version varchar(50)
 );
 
@@ -216,10 +317,20 @@ drop table if exists rule_result_summary;
 create table rule_result_summary
 (
  rule_uuid varchar(50),
+  rule_version varchar(50),
   rule_name varchar(50),
   entity_type varchar(50),
   entity_id varchar(50),
     score decimal(22,1),
-  rule_version varchar(50),
   version varchar(50)
 );
+
+drop table if exists security;
+create table security(	
+	security_id int(10),
+	security_symbol varchar(50),
+	ric_code varchar(50),
+	security_description varchar(50),
+	version int(10));
+
+	 
--- framework-web/deploy/mysql/aml/dq_rule_summary.sql
+++ framework-web/deploy/mysql/aml/dq_rule_summary.sql
@@ -1,17 +1,18 @@
 drop table if exists dq_rule_summary;
  create table dq_rule_summary (
-    rule_uuid varchar(50),
-    rule_version integer(10),
-    rule_name varchar(50),
-    datapod_uuid varchar(50),
-    datapod_version integer(10),
-    datapod_name varchar(50),
-    all_check_pass varchar(1),
-    total_row_count integer(10),
-    total_pass_count integer(10),
-    total_fail_count integer(10),
-    threshold_type varchar(50),
-    threshold_limit integer(3),
-    threshold_ind varchar(5),
-    score decimal(22,1),
-    version integer(10));
+    rule_uuid        varchar(50),
+ rule_version     int(10),
+ rule_name        varchar(50),
+ datapod_uuid     varchar(50),
+ datapod_version  int(10),
+ datapod_name     varchar(50),
+ all_check_pass   varchar(1),
+ total_row_count  int(10),
+ total_pass_count int(10),
+ total_fail_count int(10),
+ threshold_type   varchar(50),
+ threshold_limit  int(3),
+ threshold_ind    varchar(5),
+ score            int(3),
+ version          int(10));
+
