54e6444e2cc0bc1c5edacebf9e0c4ee29b3218ad
jkumar88 <goyaljitender88@gmail.com>
2019-03-20 15:41:33 +0530

    add dependsOn hiperlink into jobmonitring page

--- framework-web/src/main/webapp/app/js/controllers/profileExecController.js
+++ framework-web/src/main/webapp/app/js/controllers/profileExecController.js
@@ -45,6 +45,16 @@ JobMonitoringModule.controller('DetailProfileExecController', function ($filter,
       statusList[i] = response.statusList[i].stage + "-" + d;
     }
     $scope.statusList = statusList;
+    var dependsOnlist = [];
+    if (response.dependsOn != null) {
+        var dependsOn = {};
+        dependsOn.type = response.dependsOn.ref.type;
+        dependsOn.name = response.dependsOn.ref.type + "-"+response.dependsOn.ref.name;
+        dependsOn.uuid = response.dependsOn.ref.uuid;
+        dependsOn.version = response.dependsOn.ref.version;
+        dependsOnlist[0] = dependsOn;
+    }
+    $scope.dependsOnlist=dependsOnlist;
   };
   var onError=function(){
     $scope.isEditInprogess=false;
@@ -56,6 +66,24 @@ JobMonitoringModule.controller('DetailProfileExecController', function ($filter,
     $scope.showGraphDiv = true;
 
   }
+
+  $scope.onShowDetailDepOn=function(data){
+    $rootScope.previousState = {};
+    $rootScope.previousState.name = dagMetaDataService.elementDefs['profileexec'].detailState;
+    $rootScope.previousState.params = {};
+    $rootScope.previousState.params.id = $stateParams.id;
+    $rootScope.previousState.params.mode = true;
+    var type = data.type
+    var uuid = data.uuid
+    var stageName = dagMetaDataService.elementDefs[type.toLowerCase()].detailState;
+    var stageparam = {};
+    stageparam.id = uuid;
+    stageparam.version = data.version;
+    stageparam.mode = true;
+    stageparam.returnBack = true;
+    $state.go(stageName, stageparam);
+}
+
 
   $scope.showExecPage = function () {
     $scope.showExec = true
