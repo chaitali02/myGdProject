8b8970fbcecd6753da238440332fb8374af21542
Vaibhav <vaibhavakatkar@gmail.com>
2019-03-24 21:13:47 +0530

    enhanced script

--- framework-web/deploy/mysql/db/counts.sql
+++ framework-web/deploy/mysql/db/counts.sql
@@ -1,20 +1,22 @@
 select tabname,cnt from
 (
-select 'dq_result_summary' as tabname, count(*) as cnt from framework.dq_result_summary
+select 'dp_result_summary' as tabname, count(*) as cnt from framework_db.dp_result_summary
 union
-select 'dq_result_detail' as tabname, count(*) as cnt from framework.dq_result_detail
+select 'dq_result_summary' as tabname, count(*) as cnt from framework_db.dq_result_summary
 union
-select 'equity_orders' as tabname, count(*) as cnt from framework.equity_orders
+select 'dq_result_detail' as tabname, count(*) as cnt from framework_db.dq_result_detail
 union
-select 'equity_executions' as tabname, count(*) as cnt from framework.equity_executions
+select 'equity_orders' as tabname, count(*) as cnt from framework_db.equity_orders
 union
-select 'security' as tabname, count(*) as cnt from framework.security
+select 'equity_executions' as tabname, count(*) as cnt from framework_db.equity_executions
 union
-select 'rule_result_detail' as tabname, count(*) as cnt from framework.rule_result_detail
+select 'security' as tabname, count(*) as cnt from framework_db.security
 union
-select 'rule_result_summary' as tabname, count(*) as cnt from framework.rule_result_summary
+select 'rule_result_detail' as tabname, count(*) as cnt from framework_db.rule_result_detail
 union
-select 'account_summary_daily' as tabname, count(*) as cnt from framework.account_summary_daily
+select 'rule_result_summary' as tabname, count(*) as cnt from framework_db.rule_result_summary
+union
+select 'account_summary_daily' as tabname, count(*) as cnt from framework_db.account_summary_daily
 
 ) t1
 order by tabname;
--- framework-web/deploy/mysql/db/create_all.sql
+++ framework-web/deploy/mysql/db/create_all.sql
@@ -3,15 +3,15 @@ DROP TABLE IF EXISTS equity_executions;
 
 CREATE TABLE equity_executions
   ( 
-     trade_execution_identifier              VARCHAR(50), 
-     trade_execution_event_number            VARCHAR(50), 
+     trade_execution_identifier              INT(10), 
+     trade_execution_event_number            INT(2), 
      trade_execution_event_type_code         VARCHAR(50), 
      trade_execution_date                    VARCHAR(50), 
      trade_execution_time                    VARCHAR(50), 
      trade_execution_event_date              VARCHAR(50), 
      trade_execution_event_time              VARCHAR(50), 
      trade_allocation_relationship_code      VARCHAR(50), 
-     allocated_trade_execution_id            VARCHAR(50), 
+     allocated_trade_execution_id            INT(10), 
      allocated_trade_execution_date          VARCHAR(50), 
      trade_purpose                           VARCHAR(50), 
      firm_identifier                         VARCHAR(50), 
@@ -68,7 +68,7 @@ CREATE TABLE equity_executions
      settlement_type                         VARCHAR(50), 
      settlement_currency                     VARCHAR(50), 
      settlement_instruction                  VARCHAR(50), 
-     order_identifier                        VARCHAR(50), 
+     order_identifier                        INT(10), 
      order_placement_date                    VARCHAR(50), 
      order_execution_sequence_number         VARCHAR(50), 
      source_system_order_remaining_unit_qty  VARCHAR(50), 
@@ -80,48 +80,23 @@ CREATE TABLE equity_executions
      cancellation_or_correction_reason       VARCHAR(50), 
      business_domain                         VARCHAR(50), 
      jurisdiction                            VARCHAR(50), 
-     source_system                           VARCHAR(50) 
-  ); 
-
-DROP TABLE IF EXISTS equity_executions; 
-
-CREATE TABLE equity_executions 
-  ( 
-     execution_id         INT(10), 
-     version_id           INT(10), 
-     execution_event_type VARCHAR(50), 
-     order_id             INT(10), 
-     execution_date       VARCHAR(50), 
-     execution_time       VARCHAR(50), 
-     execution_event_date VARCHAR(50), 
-     execution_event_time VARCHAR(50), 
-     execution_price      DOUBLE, 
-     trading_account_id   INT(10), 
-     position_trader_id   INT(10), 
-     security_id          INT(10), 
-     security_symbol      VARCHAR(50), 
-     ric_code             VARCHAR(50), 
-     security_description VARCHAR(100), 
-     client_account_id    INT(10), 
-     parent_order_id      INT(10), 
-     quantity             DOUBLE, 
-     reason               VARCHAR(500), 
-     version              INT(10) 
+     source_system                           VARCHAR(50),
+	 PRIMARY KEY(trade_execution_identifier,trade_execution_event_number)     
   ); 
 
 DROP TABLE IF EXISTS equity_orders; 
 
 CREATE TABLE equity_orders
   ( 
-     order_identifier                        VARCHAR(50), 
-     order_event_number                      VARCHAR(50), 
+     order_identifier                        INT(10), 
+     order_event_number                      INT(2), 
      order_placement_date                    VARCHAR(50), 
-     parent_order_id                         VARCHAR(50), 
+     parent_order_id                         INT(10), 
      parent_order_placement_date             VARCHAR(50), 
      root_order_identifier                   VARCHAR(50), 
      root_order_placement_date               VARCHAR(50), 
      order_allocation_relationship_code      VARCHAR(50), 
-     allocated_order_id                      VARCHAR(50), 
+     allocated_order_id                      INT(10), 
      allocated_order_placement_date          VARCHAR(50), 
      order_event_type_cd                     VARCHAR(50), 
      order_event_date                        VARCHAR(50), 
@@ -188,38 +163,9 @@ CREATE TABLE equity_orders
      replaced_order_placement_date           VARCHAR(50), 
      business_domain                         VARCHAR(50), 
      jurisdiction                            VARCHAR(50), 
-     source_system                           VARCHAR(50) 
-  ); 
-
-DROP TABLE IF EXISTS equity_orders; 
-
-CREATE TABLE equity_orders 
-  ( 
-     order_id             INT(10), 
-     version_id           INT(10), 
-     order_event_type     VARCHAR(50), 
-     order_placement_date VARCHAR(50), 
-     order_placement_time VARCHAR(50), 
-     order_event_date     VARCHAR(50), 
-     order_event_time     VARCHAR(50), 
-     time_in_force        VARCHAR(50), 
-     order_type           VARCHAR(50), 
-     limit_price          DOUBLE, 
-     stop_price           DOUBLE, 
-     trading_account_id   INT(10), 
-     sales_trader_id      INT(10), 
-     position_trader_id   INT(10), 
-     security_id          INT(10), 
-     security_symbol      VARCHAR(50), 
-     ric_code             VARCHAR(50), 
-     security_description VARCHAR(100), 
-     client_account_id    INT(10), 
-     parent_order_id      INT(10), 
-     route_destination    VARCHAR(50), 
-     quantity             DOUBLE, 
-     reason               VARCHAR(500), 
-     version              INT(10) 
-  ); 
+     source_system                           VARCHAR(50),
+	 PRIMARY KEY(order_identifier,order_event_number)     
+  );
 DROP TABLE IF EXISTS security; 
 
 CREATE TABLE security 
