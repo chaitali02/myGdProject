83034d476f9cac411a029dd44ebdfd67cb9ffd13
chaitali <chaitalidhamande@gmail.com>
2019-04-10 20:06:49 +0530

    added POJOs

---
+++ framework-web/src/main/webapp/newapp/src/app/metadata/domainIO/domain.expMetIO.ts
@@ -0,0 +1,7 @@
+export class ExpMetIO{
+    mettype : any;
+    ismetlhsSimple : any;
+    ismetFormula : any;
+    metlhsvalue : any;
+    metFormula :any;
+}
---
+++ framework-web/src/main/webapp/newapp/src/app/metadata/domainIO/domain.notMetTypeIO.ts
@@ -0,0 +1,7 @@
+export class ExpNotMetIO{
+    notmettype : any;
+    isnotmetlhsSimple : any;
+    isnotmetFormula : any;
+    notmetlhsvalue : any;
+    notmetFormula :any;
+}
