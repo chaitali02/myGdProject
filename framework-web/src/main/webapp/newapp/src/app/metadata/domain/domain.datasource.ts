5a95434b43d85eec6ce4c92c1ef15ad36f0149da
CDevendra <deven.chandode@gmail.com>
2019-04-19 16:04:53 +0530

    enhanced Datasource component

--- framework-web/src/main/webapp/newapp/src/app/metadata/domain/domain.datasource.ts
+++ framework-web/src/main/webapp/newapp/src/app/metadata/domain/domain.datasource.ts
@@ -2,14 +2,17 @@ import { BaseEntity } from './domain.baseEntity';
 
 export class Datasource extends BaseEntity{
 
-   // private type : String;
-	private access : String;
-	private driver : String;
-	private host : String;
-	private dbname : String;
-	private port : String;
-	private username : String;
-	private password : String;
-	private path : String;
-	private subType : String;
+   type : String;
+	access : String;
+	driver : String;
+	host : String;
+	dbname : String;
+	port : String;
+	username : String;
+	password : String;
+	path : String;
+	subType : String;
+	sessionParameters: String;
+	sid: String;
+	
 }
