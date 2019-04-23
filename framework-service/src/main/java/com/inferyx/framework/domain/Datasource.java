bdb82ab7b92e19dbc7eb80e3ba3f42a30730ed64
ganeshsonawane06 <ganeshm.sonawane06@gmail.com>
2019-04-23 17:37:40 +0530

    new property FormatType and removed enum DataSourceType

--- framework-service/src/main/java/com/inferyx/framework/domain/Datasource.java
+++ framework-service/src/main/java/com/inferyx/framework/domain/Datasource.java
@@ -12,11 +12,10 @@ package com.inferyx.framework.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="datasource")
-public class Datasource extends BaseEntity{
+import com.inferyx.framework.enums.FormatType;
 
-	public enum DataSourceType {SPARK,FILE,HIVE,ORACLE,IMPALA,MYSQL,STREAM};
-	
+@Document(collection = "datasource")
+public class Datasource extends BaseEntity {
 	private String type;
 	private String access;
 	private String driver;
@@ -29,77 +28,138 @@ public class Datasource extends BaseEntity{
 	private String subType;
 	private String sessionParameters;
 	private String sid;
-	
+	private String url;
+	private FormatType format; // JSON, CSV, PSV, TSV, XML, EXCEL
+
 	public String getSid() {
 		return sid;
 	}
+
 	public void setSid(String sid) {
 		this.sid = sid;
 	}
+
 	public String getSessionParameters() {
 		return sessionParameters;
 	}
+
 	public void setSessionParameters(String sessionParameters) {
 		this.sessionParameters = sessionParameters;
 	}
+
 	public String getSubType() {
 		return subType;
 	}
+
 	public void setSubType(String subType) {
 		this.subType = subType;
 	}
+
 	public String getPath() {
 		return path;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getAccess() {
 		return access;
 	}
+
 	public void setAccess(String access) {
 		this.access = access;
 	}
+
 	public String getDriver() {
 		return driver;
 	}
+
 	public void setDriver(String driver) {
 		this.driver = driver;
 	}
+
 	public String getHost() {
 		return host;
 	}
+
 	public void setHost(String host) {
 		this.host = host;
 	}
+
 	public String getDbname() {
 		return dbname;
 	}
+
 	public void setDbname(String dbname) {
 		this.dbname = dbname;
 	}
+
 	public String getPort() {
 		return port;
 	}
+
 	public void setPort(String port) {
 		this.port = port;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
-	}	
+	}
+
+	/**
+	 * @Ganesh
+	 *
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @Ganesh
+	 *
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	/**
+	 * @Ganesh
+	 *
+	 * @return the format
+	 */
+	public FormatType getFormat() {
+		return format;
+	}
+
+	/**
+	 * @Ganesh
+	 *
+	 * @param format the format to set
+	 */
+	public void setFormat(FormatType format) {
+		this.format = format;
+	}
 }
